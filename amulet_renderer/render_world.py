6b78317b17e192f1327a5c9e49bd82668e9d3bd6
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-12-12 12:52:09 +0000

    Implemented architecture to allow chunk loading

--- amulet_renderer/render_world.py
+++ amulet_renderer/render_world.py
@@ -1,27 +1,50 @@
 from OpenGL.GL import *
-from amulet_renderer import shaders
 import numpy
-from typing import TYPE_CHECKING, Dict, Tuple
+from typing import TYPE_CHECKING, Dict, Tuple, Generator
 if TYPE_CHECKING:
     from amulet.api.world import World
 import random
+import itertools
+
+from amulet_renderer import shaders
 
 
 class RenderWorld:
     def __init__(self, world: 'World'):
         self.world = world
-        self.camera_matrix = None
-        self.loaded_render_chunks: Dict[Tuple[int, int], 'RenderChunk'] = {
-            (0, 0): RenderChunk(),
-            (0, 1): RenderChunk()
-        }
+        self.camera_location = (0, 0, 0)
+        self.render_distance = 10
+        self._loaded_render_chunks: Dict[Tuple[int, int], 'RenderChunk'] = {}
+
+    def _get_render_chunk(self, chunk_coords: Tuple[int, int]) -> 'RenderChunk':
+        if not chunk_coords in self._loaded_render_chunks:
+            self._loaded_render_chunks[chunk_coords] = RenderChunk()  # TODO: get the chunk data from the world
+        return self._loaded_render_chunks[chunk_coords]
+
+    def chunk_coords(self) -> Generator[Tuple[int, int], None, None]:
+        """Get all of the chunks to draw/load"""
+        x, z = self.camera_location[0] // 16, self.camera_location[2] // 16
+        chunks = itertools.product(
+            range(x-self.render_distance, x+self.render_distance),
+            range(z-self.render_distance, z+self.render_distance)
+        )
+
+        for chunk in sorted(chunks, key=lambda c: (c[0]-x)**2 + (c[1]-x)**2):
+            yield chunk
 
     def draw(self):
         # draw all chunks within render distance
-        for chunk in self.loaded_render_chunks.values():
-            chunk.draw()
+        load_chunks = []
+        for chunk_coords in self.chunk_coords():
+            if chunk_coords in self._loaded_render_chunks:
+                chunk = self._loaded_render_chunks[chunk_coords]
+                chunk.draw()
+            else:
+                load_chunks.append(chunk_coords)
 
         # generate chunks
+        if load_chunks:
+            self._loaded_render_chunks[load_chunks[0]] = RenderChunk()
 
 
 class RenderChunk:
