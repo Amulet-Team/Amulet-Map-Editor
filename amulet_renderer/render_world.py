04592c224737c81b0a5d4e456277be042b51beb4
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-12-12 13:52:23 +0000

    Restructuring for adding chunk data

--- amulet_renderer/render_world.py
+++ amulet_renderer/render_world.py
@@ -3,6 +3,7 @@ import numpy
 from typing import TYPE_CHECKING, Dict, Tuple, Generator
 if TYPE_CHECKING:
     from amulet.api.world import World
+    from amulet.api.chunk import Chunk
 import random
 import itertools
 
@@ -13,12 +14,15 @@ class RenderWorld:
     def __init__(self, world: 'World'):
         self.world = world
         self.camera_location = (0, 0, 0)
-        self.render_distance = 10
+        self.render_distance = 1
         self._loaded_render_chunks: Dict[Tuple[int, int], 'RenderChunk'] = {}
+        self.shaders = {
+            'render_chunk': shaders.load_shader('render_chunk')
+        }
 
     def _get_render_chunk(self, chunk_coords: Tuple[int, int]) -> 'RenderChunk':
         if not chunk_coords in self._loaded_render_chunks:
-            self._loaded_render_chunks[chunk_coords] = RenderChunk()  # TODO: get the chunk data from the world
+            self._loaded_render_chunks[chunk_coords] = RenderChunk(self, chunk_coords)  # TODO: get the chunk data from the world
         return self._loaded_render_chunks[chunk_coords]
 
     def chunk_coords(self) -> Generator[Tuple[int, int], None, None]:
@@ -44,15 +48,16 @@ class RenderWorld:
 
         # generate chunks
         if load_chunks:
-            self._loaded_render_chunks[load_chunks[0]] = RenderChunk()
+            self._loaded_render_chunks[load_chunks[0]] = RenderChunk(self, load_chunks[0])
 
 
 class RenderChunk:
-    def __init__(self):
+    def __init__(self, render_world: RenderWorld, chunk_coords: Tuple[int, int]):
         # the chunk geometry is stored in chunk space (floating point)
         # at shader time it is transformed by the players transform
-
-        self.main_shader = shaders.load_shader('render_chunk')
+        self.render_world = render_world
+        self.coords = chunk_coords
+        self.chunk = self.render_world.world.get_chunk(*chunk_coords)
         self.vao = glGenVertexArrays(1)
         self.create_geometry()
 
@@ -63,12 +68,12 @@ class RenderChunk:
         # perhaps in the future (if face merging is added) a way to specify texture bounds
         # normals
 
-        offset = random.random()
-        print(offset)
         # triangle   vertices      colors
-        triangle = [offset -0.5 + -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-                    offset -0.5 + 0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-                    offset -0.5 + 0.0, 0.5, 0.0, 0.0, 0.0, 1.0]
+        triangle = [
+            self.coords[0] + -0.5, -0.5, self.coords[1] + 0.0, 1.0, 0.0, 0.0,
+            self.coords[0] -0.5 + 0.5, -0.5, self.coords[1] + 0.0, 0.0, 1.0, 0.0,
+            self.coords[0] -0.5 + 0.0, 0.5, self.coords[1] + 0.0, 0.0, 0.0, 1.0
+        ]
 
         triangle = numpy.array(triangle, dtype=numpy.float32)
 
@@ -85,7 +90,7 @@ class RenderChunk:
         glBindVertexArray(0)
 
     def draw(self):
-        glUseProgram(self.main_shader)
+        glUseProgram(self.render_world.shaders['render_chunk'])
         glBindVertexArray(self.vao)
         glDrawArrays(GL_TRIANGLES, 0, 3)
 
