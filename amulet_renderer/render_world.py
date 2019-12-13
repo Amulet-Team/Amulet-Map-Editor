736b6236be94d937d839aceca01bcefc110900d0
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-12-13 15:21:11 +0000

    Added chunk offset to transformation matrix

--- amulet_renderer/render_world.py
+++ amulet_renderer/render_world.py
@@ -1,13 +1,15 @@
 from OpenGL.GL import *
 import numpy
-from typing import TYPE_CHECKING, Dict, Tuple, Generator
-if TYPE_CHECKING:
-    from amulet.api.world import World
-    from amulet.api.chunk import Chunk
+from typing import TYPE_CHECKING, Dict, Tuple, Generator, Union
 import math
 import itertools
 
 from amulet_renderer import shaders
+
+from amulet.api.errors import ChunkDoesNotExist
+if TYPE_CHECKING:
+    from amulet.api.world import World
+    from amulet.api.chunk import Chunk
 
 
 class RenderWorld:
@@ -16,23 +18,19 @@ class RenderWorld:
         self.projection = [45.0, 4/3, 0.1, 1000.0]
         self.camera_location = [1, 0, 0]
         self.camera_rotation = [0, 0]
-        self.render_distance = 1
-        self._loaded_render_chunks: Dict[Tuple[int, int], 'RenderChunk'] = {}
+        self.render_distance = 10
+        self._loaded_render_chunks: Dict[Tuple[int, int], Union['RenderChunk', None]] = {}
         self.shaders = {
             'render_chunk': shaders.load_shader('render_chunk')
         }
 
     @property
     def transformation_matrix(self) -> numpy.ndarray:
-        fovy, aspect, z_near, z_far = self.projection
-        f = 1/math.tan(fovy/2)
-
         # camera translation
         transformation_matrix = numpy.eye(4, dtype=numpy.float64)
-        transformation_matrix[3, :3] = self.camera_location
+        transformation_matrix[3, :3] = numpy.array(self.camera_location) * -1
 
         # rotations
-
         theta = math.radians(self.camera_rotation[0])
         c = math.cos(theta)
         s = math.sin(theta)
@@ -66,6 +64,9 @@ class RenderWorld:
         transformation_matrix = numpy.matmul(transformation_matrix, y_rot)
 
         # camera projection
+        fovy, aspect, z_near, z_far = self.projection
+        fovy = math.radians(fovy)
+        f = 1 / math.tan(fovy / 2)
         projection = numpy.array(
             [
                 [f/aspect, 0, 0, 0],
@@ -80,9 +81,14 @@ class RenderWorld:
 
         return transformation_matrix
 
-    def _get_render_chunk(self, chunk_coords: Tuple[int, int]) -> 'RenderChunk':
-        if not chunk_coords in self._loaded_render_chunks:
-            self._loaded_render_chunks[chunk_coords] = RenderChunk(self, chunk_coords)  # TODO: get the chunk data from the world
+    def _get_render_chunk(self, chunk_coords: Tuple[int, int]) -> Union['RenderChunk', None]:
+        if chunk_coords not in self._loaded_render_chunks:
+            try:
+                chunk = self.world.get_chunk(*chunk_coords)
+            except ChunkDoesNotExist:
+                self._loaded_render_chunks[chunk_coords] = None
+            else:
+                self._loaded_render_chunks[chunk_coords] = RenderChunk(self, chunk_coords, chunk)  # TODO: get the chunk data from the world
         return self._loaded_render_chunks[chunk_coords]
 
     def chunk_coords(self) -> Generator[Tuple[int, int], None, None]:
@@ -102,22 +108,24 @@ class RenderWorld:
         for chunk_coords in self.chunk_coords():
             if chunk_coords in self._loaded_render_chunks:
                 chunk = self._loaded_render_chunks[chunk_coords]
+                if chunk is None:
+                    continue
                 chunk.draw(transformation_matrix)
             else:
                 load_chunks.append(chunk_coords)
 
         # generate chunks
         if load_chunks:
-            self._loaded_render_chunks[load_chunks[0]] = RenderChunk(self, load_chunks[0])
+            self._get_render_chunk(load_chunks[0])
 
 
 class RenderChunk:
-    def __init__(self, render_world: RenderWorld, chunk_coords: Tuple[int, int]):
+    def __init__(self, render_world: RenderWorld, chunk_coords: Tuple[int, int], chunk: 'Chunk'):
         # the chunk geometry is stored in chunk space (floating point)
         # at shader time it is transformed by the players transform
         self.render_world = render_world
         self.coords = chunk_coords
-        self.chunk = self.render_world.world.get_chunk(*chunk_coords)
+        self.chunk = chunk
         self.vao = glGenVertexArrays(1)
         self.create_geometry()
 
@@ -130,9 +138,9 @@ class RenderChunk:
 
         # triangle   vertices      colors
         triangle = [
-            self.coords[0] + -0.5, -0.5, self.coords[1] + 0.0, 1.0, 0.0, 0.0,
-            self.coords[0] -0.5 + 0.5, -0.5, self.coords[1] + 0.0, 0.0, 1.0, 0.0,
-            self.coords[0] -0.5 + 0.0, 0.5, self.coords[1] + 0.0, 0.0, 0.0, 1.0
+            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+            -0.5 + 0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+            -0.5 + 0.0, 0.5, 0.0, 0.0, 0.0, 1.0
         ]
 
         triangle = numpy.array(triangle, dtype=numpy.float32)
@@ -152,8 +160,15 @@ class RenderChunk:
     def draw(self, transformation_matrix: numpy.ndarray):
         shader = self.render_world.shaders['render_chunk']
         glUseProgram(shader)
+
+        chunk_translation = numpy.eye(4, dtype=numpy.float64)
+        chunk_translation[3, [0, 2]] = numpy.array(self.coords) * 16
+
+        transformation_matrix = numpy.matmul(chunk_translation, transformation_matrix)
+
         trm_mat_loc = glGetUniformLocation(shader, "transformation_matrix")
         glUniformMatrix4fv(trm_mat_loc, 1, GL_FALSE, transformation_matrix.astype(numpy.float32))
+
         glBindVertexArray(self.vao)
         glDrawArrays(GL_TRIANGLES, 0, 3)
 
