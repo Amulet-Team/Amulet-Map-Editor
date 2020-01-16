d0964221a76cb2a45ff31cf5cffdc02419363beb
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-16 16:37:32 +0000

    Added some modular classes to deal with world loading

--- amulet_map_editor/util/world_select.py
+++ amulet_map_editor/util/world_select.py
@@ -1,20 +1,133 @@
-class WorldUI:
+import os
+import wx
+from amulet_map_editor import lang, config
+import amulet
+from amulet_map_editor.util import wx_util
+
+# Windows 	%APPDATA%\.minecraft
+# macOS 	~/Library/Application Support/minecraft
+# Linux 	~/.minecraft
+
+java_dir = os.path.join(os.getenv('APPDATA'), '.minecraft', 'saves')
+bedrock_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'Packages', 'Microsoft.MinecraftUWP_8wekyb3d8bbwe', 'LocalState', 'games', 'com.mojang', 'minecraftWorlds')
+# TODO: handle other OSs
+
+
+minecraft_world_paths = {
+    lang.get('java_platform'): java_dir,
+    lang.get('bedrock_platform'): bedrock_dir
+}
+
+
+class WorldUI(wx_util.SimplePanel):
     # UI element that holds the world image, name and description
-    pass
+    def __init__(self, parent: 'WorldDirectoryUI', path: str):
+        super(WorldUI, self).__init__(
+            parent,
+            wx.HORIZONTAL
+        )
+
+        world = amulet.load_format(path)
+
+        parent.sizer.Add(
+            self,
+            0,
+            wx.ALL,
+            5
+        )
+
+        img = wx.Image(
+            world.world_image_path,
+            wx.BITMAP_TYPE_ANY
+        ).ConvertToBitmap()
+
+        self.img = wx.StaticBitmap(
+            self,
+            wx.ID_ANY,
+            img,
+            (0, 0),
+            (128, 128)
+        )
+        # self.img.SetScaleMode(2)
+        self.sizer.Add(self.img)
+
+        self.world_name = wx.StaticText(
+            self,
+            wx.ID_ANY,
+            world.world_name,
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0,
+        )
+        self.sizer.Add(self.world_name)
 
 
-class WorldUIButton:
+class WorldUIButton(WorldUI):
     # a button that wraps around WorldUI so that WorldUI can be used without the button functionality
-    pass
+    def __init__(self, parent: 'WorldDirectoryUI', path: str):
+        super(WorldUIButton, self).__init__(
+            parent,
+            path
+        )
+        self.Bind(wx.EVT_LEFT_UP, self._say_hi)
+        self.img.Bind(wx.EVT_LEFT_UP, self._say_hi)
+        self.world_name.Bind(wx.EVT_LEFT_UP, self._say_hi)
+
+    def _say_hi(self, evt):
+        print('hi')
 
 
-class WorldDirectoryUI:
+class WorldDirectoryUI(wx.CollapsiblePane):
     # a drop down list of `WorldUIButton`s for a given directory
-    pass
+    def __init__(self, parent: 'WorldSelectUI', path: str, group_name: str):
+        super(WorldDirectoryUI, self).__init__(
+            parent,
+            wx.ID_ANY,
+            group_name
+        )
+        self.parent = parent
+        parent.add_object(self)
+        self.Bind(wx.EVT_COLLAPSIBLEPANE_CHANGED, self.eval_layout)
+
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.sizer)
+
+        panel = self.GetPane()
+        panel.sizer = wx.BoxSizer(wx.VERTICAL)
+        panel.SetSizer(panel.sizer)
+
+        self.worlds = []
+
+        for world in os.listdir(path):
+            world_path = os.path.join(path, world)
+            if os.path.isdir(world_path):
+                try:
+                    self.worlds.append(WorldUIButton(panel, world_path))
+                except:
+                    pass
+
+    def eval_layout(self, evt):
+        self.Layout()
+        self.parent.Layout()
 
 
-class WorldSelectUI:
+class WorldSelectUI(wx_util.SimplePanel):
     # a frame containing a refresh button for the UI, a sort order for the worlds
     # and a vertical list of `WorldDirectoryUI`s for each directory
     # perhaps also a select directory option
-    pass
+    def __init__(self, parent):
+        super(WorldSelectUI, self).__init__(
+            parent
+        )
+
+        self.dirs = {}
+        self._reload()
+
+        self.Layout()
+
+    def _reload(self):
+        # might need to remove them from the sizer first
+        self.dirs.clear()
+        for group_name, directory in minecraft_world_paths.items():
+            if os.path.isdir(directory):
+                self.dirs[directory] = WorldDirectoryUI(self, directory, group_name)
