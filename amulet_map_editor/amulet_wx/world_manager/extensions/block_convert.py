f7b43bd26f6735a986e215adb41baf43bc4ad3e5
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-02-25 18:58:07 +0000

    Added a world tool to test the block selection

---
+++ amulet_map_editor/amulet_wx/world_manager/extensions/block_convert.py
@@ -0,0 +1,25 @@
+from amulet_map_editor.amulet_wx.block_select import VersionSelect
+from amulet_map_editor.amulet_wx.world_manager import BaseWorldTool
+from amulet.api.world import World
+import wx
+from concurrent.futures import ThreadPoolExecutor
+
+thread_pool_executor = ThreadPoolExecutor(max_workers=1)
+work_count = 0
+
+
+class ConvertExtension(BaseWorldTool):
+    def __init__(self, container, world: World):
+        super(ConvertExtension, self).__init__(
+            container
+        )
+        self.world = world
+
+        self._version_select = VersionSelect(self, world.world_wrapper.translation_manager)
+        self.add_object(self._version_select, 0, wx.EXPAND)
+
+
+export = {
+    "name": "Block Convert",
+    "ui": ConvertExtension
+}
