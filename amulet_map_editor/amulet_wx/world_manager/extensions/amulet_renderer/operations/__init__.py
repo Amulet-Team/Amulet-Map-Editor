8b15e7f9e1d78c07342a72037fa7f4f4fe32697e
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-02 13:31:11 +0000

    Slightly revised plugin format

--- amulet_map_editor/amulet_wx/world_manager/extensions/amulet_renderer/operations/__init__.py
+++ amulet_map_editor/amulet_wx/world_manager/extensions/amulet_renderer/operations/__init__.py
@@ -10,12 +10,20 @@ export = {
     "v": 1,  # a version 1 plugin
     "name": "Plugin Name",  # the name of the plugin
     "operation": clone,  # the actual function to call when running the plugin
-    "inputs": ["src_box", "dst_box", "options"],  # the inputs to give to the plugin
-
+    "inputs": [],  # a list of inputs to give to the plugin. World class is passed in as the first and these following
+        # possible inputs (max one from each list)
+        ["src_box"]  # the box selected by the user
+        [
+            "dst_box"  # the destination selected by the user (box same size of src_box. "src_box" is required). The presence of this triggers the UI to enable the functionality
+            "dst_box_multiple"  # a list of destination boxes selected by the user (boxes same size of src_box. "src_box" is required). The presence of this triggers the UI to enable the functionality
+        ]
+        [
+            "options",  # "options" key must exist
+            "wxoptions"  # "wxoptions" key must exist
+        ]
     "options": {},  # a simple system of defining options from which a simple UI can be created
     "wxoptions": wxpanel,  # a more complex system allowing users to work directly with wx
-    # only one of the above should be present. The result is passed to the options slot if present (this won't be used if it isn't)
-    "features": ["src_box", "dst_box"]  # options beyond a simple UI eg selection box
+    # The result is passed to the related slot in inputs
 }
 """
 
