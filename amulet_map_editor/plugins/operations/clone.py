3809eb46269e3eec4b762e90e48a964c2bc4a0f2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2020-03-14 15:05:50 +0000

    Added clone plugin using the new methods and UI

--- amulet_map_editor/plugins/operations/clone.py
+++ amulet_map_editor/plugins/operations/clone.py
@@ -1,8 +1,21 @@
-from amulet.operations.clone import clone
+from typing import TYPE_CHECKING
+
+from amulet.api.selection import Selection
+from amulet.api.structure import Structure
+
+if TYPE_CHECKING:
+    from amulet.api.world import World
+
+
+def paste(world: "World", source: Selection, structure: Structure, dst: dict):
+    dst_location = (dst.get('x', 0), dst.get('y', 0), dst.get('z', 0))
+    for src_chunk, src_slices, _, (dst_cx, dst_cz), dst_slices, _ in structure.get_moved_chunk_slices(dst_location):
+        world.get_chunk(dst_cx, dst_cz).blocks[dst_slices] = src_chunk.blocks[src_slices]
+
 
 export = {
     "v": 1,  # a version 1 plugin
     "name": "Clone",  # the name of the plugin
-    "operation": clone,  # the actual function to call when running the plugin
-    "inputs": ["src_box", "dst_box"]  # the inputs to give to the plugin
+    "operation": paste,  # the actual function to call when running the plugin
+    "inputs": ["src_box", "structure", "dst_box"]  # the inputs to give to the plugin
 }
