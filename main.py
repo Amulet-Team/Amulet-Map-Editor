999db94e2b98f61c4dcde8166b8b86477196741c
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-04-17 13:41:25 +0100

    Added a try except for the whole program
    Also log the crash reason

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import wx
 from amulet_map_editor.amulet_ui import AmuletMainWindow
-from amulet_map_editor import config
+from amulet_map_editor import config, log
+import traceback
 
 if __name__ == "__main__":
-    app = wx.App()
-    frame = AmuletMainWindow(None)
-    app.MainLoop()
+    try:
+        app = wx.App()
+        frame = AmuletMainWindow(None)
+        app.MainLoop()
+    except Exception as e:
+        log.critical(f'Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue. \n{traceback.format_exc()}')
+        input("Press ENTER to continue.")
     config.save()
