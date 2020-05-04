0664a39d9df2d9c6d50b5549a0235af667ececc6
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-05-04 09:19:05 +0100

    cleaned up log

--- amulet_map_editor/util/log.py
+++ amulet_map_editor/util/log.py
@@ -3,21 +3,19 @@ import sys
 import os
 
 log = logging.getLogger("amulet_map_editor")
-log.setLevel(logging.INFO)
+log_level = logging.DEBUG if "amulet-debug" in sys.argv else logging.INFO
+
+log.setLevel(log_level)
 
 _formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 os.makedirs("./logs", exist_ok=True)
 _log_file = logging.FileHandler("./logs/amulet_map_editor.log", "w")
-if "amulet-debug" in sys.argv:
-    _log_file.setLevel(logging.DEBUG)
-    log.setLevel(logging.DEBUG)
-else:
-    _log_file.setLevel(logging.INFO)
+_log_file.setLevel(log_level)
 _log_file.setFormatter(_formatter)
 log.addHandler(_log_file)
 
 _log_console = logging.StreamHandler()
-_log_console.setLevel(logging.INFO)
+_log_console.setLevel(log_level)
 _log_console.setFormatter(_formatter)
 log.addHandler(_log_console)
