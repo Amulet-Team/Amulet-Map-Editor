32b28749b9d6f7b6e5974220fd3b49887d52fa11
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-05-06 19:26:42 +0100

    Fixed shader error on mac

--- amulet_map_editor/opengl/shaders/__init__.py
+++ amulet_map_editor/opengl/shaders/__init__.py
@@ -10,10 +10,19 @@ _shaders: Dict[Tuple[str, str], Any] = {}
 def get_shader(context_identifier: str, shader_name: str) -> OpenGL.GL.shaders.ShaderProgram:
     shader_key = (context_identifier, shader_name)
     if shader_key not in _shaders:
-        _shaders[shader_key] = OpenGL.GL.shaders.compileProgram(
-            _load_shader(os.path.join(shader_dir, f'{shader_name}.vert'), GL_VERTEX_SHADER),
-            _load_shader(os.path.join(shader_dir, f'{shader_name}.frag'), GL_FRAGMENT_SHADER)
-        )
+        def compile_shader():
+            return OpenGL.GL.shaders.compileProgram(
+                _load_shader(os.path.join(shader_dir, f'{shader_name}.vert'), GL_VERTEX_SHADER),
+                _load_shader(os.path.join(shader_dir, f'{shader_name}.frag'), GL_FRAGMENT_SHADER)
+            )
+        try:
+            shader = compile_shader()
+        except OpenGL.GL.shaders.ShaderValidationError: # on Mac the above fails if there is no VBO bound
+            glBindVertexArray(glGenVertexArrays(1))
+            shader = compile_shader()
+            glBindVertexArray(0)
+
+        _shaders[shader_key] = shader
     return _shaders[shader_key]
 
 
