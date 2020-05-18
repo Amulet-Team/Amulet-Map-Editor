4539d5d637983a3978e00449dab20778a75df696
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-05-18 12:32:51 +0100

    Added a schematic import and export plugin

---
+++ amulet_map_editor/plugins/export_operations/schematic.py
@@ -0,0 +1,73 @@
+from typing import TYPE_CHECKING
+import wx
+
+from amulet_map_editor.amulet_wx.block_select import PlatformSelect
+from amulet_map_editor.amulet_wx.simple import SimpleDialog
+from amulet.api.selection import SelectionGroup
+from amulet.api.errors import ChunkLoadError
+from amulet.api.data_types import Dimension
+from amulet.structure_interface.schematic import SchematicFormatWrapper
+
+if TYPE_CHECKING:
+    from amulet.api.world import World
+
+
+def export_schematic(
+    world: "World",
+    dimension: Dimension,
+    selection: SelectionGroup,
+    options: dict
+):
+    path, platform = options.get('path', None), options.get('platform', None)
+    if isinstance(path, str) and path.endswith('.schematic') and platform:
+        wrapper = SchematicFormatWrapper(path, 'w')
+        wrapper.platform = platform
+        wrapper.selection = selection
+        wrapper.translation_manager = world.translation_manager
+        wrapper.open()
+        for cx, cz in wrapper.selection.chunk_locations():
+            try:
+                chunk = world.get_chunk(cx, cz, dimension)
+                wrapper.commit_chunk(chunk, world.palette)
+            except ChunkLoadError:
+                continue
+
+        wrapper.close()
+    else:
+        raise Exception('Please specify a save location and platform in the options before running.')
+
+
+def show_ui(parent, world: "World", options: dict) -> dict:
+    dialog = SimpleDialog(parent, 'Export Schematic (legacy)')
+    file_picker = wx.FilePickerCtrl(
+        dialog,
+        path=options.get('path', ''),
+        wildcard="Schematic file (*.schematic)|*.schematic",
+        style=wx.FLP_USE_TEXTCTRL | wx.FLP_SAVE | wx.FLP_OVERWRITE_PROMPT
+    )
+    dialog.sizer.Add(file_picker, 0, wx.ALL, 5)
+    platform_define = PlatformSelect(
+        dialog,
+        world.translation_manager,
+        options.get("platform", None) or world.world_wrapper.platform,
+        allow_universal=False
+    )
+    dialog.sizer.Add(platform_define, 0)
+    dialog.Fit()
+
+    if dialog.ShowModal() == wx.ID_OK:
+        options = {
+            "path": file_picker.GetPath(),
+            "platform": platform_define.platform
+        }
+    return options
+
+
+export = {
+    "v": 1,  # a version 1 plugin
+    "name": "Export Schematic (legacy)",  # the name of the plugin
+    "features": ["src_selection", "wxoptions"],
+    "inputs": ["src_selection", "options"],  # the inputs to give to the plugin
+    "operation": export_schematic,  # the actual function to call when running the plugin
+    "wxoptions": show_ui
+}
