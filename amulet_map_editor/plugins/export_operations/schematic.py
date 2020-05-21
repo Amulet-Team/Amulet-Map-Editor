bbaf589d643923676848e72ad1c139918ccb96b4
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-05-21 18:12:47 +0100

    Added some information about the schematic format being a legacy format

--- amulet_map_editor/plugins/export_operations/schematic.py
+++ amulet_map_editor/plugins/export_operations/schematic.py
@@ -46,14 +46,30 @@ def show_ui(parent, world: "World", options: dict) -> dict:
         wildcard="Schematic file (*.schematic)|*.schematic",
         style=wx.FLP_USE_TEXTCTRL | wx.FLP_SAVE | wx.FLP_OVERWRITE_PROMPT
     )
-    dialog.sizer.Add(file_picker, 0, wx.ALL, 5)
+    dialog.sizer.Add(file_picker, 0, wx.ALL | wx.CENTER, 5)
     platform_define = PlatformSelect(
         dialog,
         world.translation_manager,
         options.get("platform", None) or world.world_wrapper.platform,
         allow_universal=False
     )
-    dialog.sizer.Add(platform_define, 0)
+    dialog.sizer.Add(platform_define, 0, wx.CENTER, 5)
+    dialog.sizer.Add(
+        wx.StaticText(
+            dialog,
+            label="""The Schematic format is a
+legacy format that can only
+save data in the numerical
+format. Anything that was
+added to the game in version
+1.13 or after will not be
+saved in the schematic file.
+
+We suggest using the Construction
+file format instead.""",
+            style=wx.ALIGN_CENTRE_HORIZONTAL
+        ), 0, wx.ALL | wx.CENTER, 5
+    )
     dialog.Fit()
 
     if dialog.ShowModal() == wx.ID_OK:
