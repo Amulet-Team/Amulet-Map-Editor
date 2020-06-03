4d988e09c8bb414eeae37d8705bcda49ff846c26
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-03 16:45:40 +0100

    Updated the waterlog operation

--- amulet_map_editor/programs/edit/plugins/stock_plugins/operations/waterlog.py
+++ amulet_map_editor/programs/edit/plugins/stock_plugins/operations/waterlog.py
@@ -1,26 +1,71 @@
+import numpy
 from typing import TYPE_CHECKING
 import wx
-import numpy
 
-from amulet.api.selection import SelectionGroup
-from amulet.api.block import Block
-from amulet.api.data_types import Dimension
 from amulet_map_editor.amulet_wx.ui.select_block import BlockDefine
-from amulet_map_editor.amulet_wx.ui.simple import SimpleDialog
+from amulet_map_editor.programs.edit.plugins import OperationUI
 
 if TYPE_CHECKING:
     from amulet.api.world import World
+    from amulet_map_editor.programs.edit.canvas.edit_canvas import EditCanvas
 
 
-def waterlog(
-    world: "World",
-    dimension: Dimension,
-    selection: SelectionGroup,
-    options: dict
-):
-    waterlog_block = options.get("fill_block", None)
-    waterlog_block: Block
-    if isinstance(waterlog_block, Block):
+class Waterlog(wx.Panel, OperationUI):
+    def __init__(
+            self,
+            parent: wx.Window,
+            canvas: "EditCanvas",
+            world: "World",
+            options_path: str
+    ):
+        wx.Panel.__init__(self, parent)
+        OperationUI.__init__(self, parent, canvas, world, options_path)
+
+        self._sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self._sizer)
+
+        options = self._load_options({})
+
+        self._block_define = BlockDefine(
+            self,
+            world.world_wrapper.translation_manager,
+            *(options.get("fill_block_options", []) or [world.world_wrapper.platform]),
+            style=wx.BORDER_SIMPLE,
+            properties_style=wx.BORDER_SIMPLE
+        )
+        self._sizer.Add(self._block_define, 0, wx.ALL | wx.ALIGN_CENTRE_HORIZONTAL, 5)
+
+        self._run_button = wx.Button(self, label="Run Operation")
+        self._run_button.Bind(wx.EVT_BUTTON, self._run_operation)
+        self._sizer.Add(self._run_button, 0, wx.ALL | wx.ALIGN_CENTRE_HORIZONTAL, 5)
+
+        self.Layout()
+
+    def _get_fill_block(self):
+        return self.world.world_wrapper.translation_manager.get_version(
+            self._block_define.platform,
+            self._block_define.version
+        ).block.to_universal(
+            self._block_define.block,
+            force_blockstate=self._block_define.force_blockstate
+        )[0]
+
+    def unload(self):
+        self._save_options({
+            "fill_block": self._get_fill_block(),
+            "fill_block_options": self._block_define.options
+        })
+
+    def _run_operation(self, _):
+        self.canvas.run_operation(
+            lambda: self._waterlog()
+        )
+
+    def _waterlog(self):
+        waterlog_block = self._get_fill_block()
+        world = self.world
+        selection = self.canvas.selection_group
+        dimension = self.canvas.dimension
         iter_count = len(list(world.get_chunk_slices(selection, dimension, True)))
         count = 0
         for chunk, slices, _ in world.get_chunk_slices(selection, dimension, True):
@@ -41,38 +86,9 @@ def waterlog(
             chunk.changed = True
             count += 1
             yield 100 * count / iter_count
-    else:
-        raise Exception('Please specify a block before running the waterlog operation')
-
-
-def show_ui(parent, world: "World", options: dict) -> dict:
-    dialog = SimpleDialog(parent, 'Waterlog')
-    block_define = BlockDefine(
-        dialog,
-        world.world_wrapper.translation_manager,
-        *(options.get("fill_block_options", []) or [world.world_wrapper.platform])
-    )
-    dialog.sizer.Add(block_define)
-    dialog.Fit()
-    if dialog.ShowModal() == wx.ID_OK:
-        options = {
-            "fill_block": world.world_wrapper.translation_manager.get_version(
-                block_define.platform,
-                block_define.version
-            ).block.to_universal(
-                block_define.block,
-                force_blockstate=block_define.force_blockstate
-            )[0],
-            "fill_block_options": block_define.options
-        }
-    return options
 
 
 export = {
-    "v": 1,  # a version 1 plugin
     "name": "Waterlog",  # the name of the plugin
-    "features": ["src_selection", "wxoptions"],
-    "inputs": ["src_selection", "options"],  # the inputs to give to the plugin
-    "operation": waterlog,  # the actual function to call when running the plugin
-    "wxoptions": show_ui
+    "operation": Waterlog,  # the actual function to call when running the plugin
 }
