4542cbdded448d0dcf4b1ab680def4c2d88eeaf7
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-08 15:57:36 +0100

    Stopped clone incrementing the undo counter

--- amulet_map_editor/programs/edit/plugins/stock_plugins/operations/clone.py
+++ amulet_map_editor/programs/edit/plugins/stock_plugins/operations/clone.py
@@ -6,6 +6,7 @@ from amulet.api.selection import SelectionGroup
 from amulet.api.data_types import Dimension, OperationReturnType
 
 from amulet_map_editor.programs.edit.plugins.api.simple_operation_panel import SimpleOperationPanel
+from amulet_map_editor.programs.edit.plugins.api.errors import OperationSilentAbort
 
 if TYPE_CHECKING:
     from amulet.api.world import World
@@ -29,6 +30,7 @@ class Clone(SimpleOperationPanel):
             world, selection, dimension
         )
         self.canvas.paste(structure)
+        raise OperationSilentAbort
 
     def unload(self):
         pass
