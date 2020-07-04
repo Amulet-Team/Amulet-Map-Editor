e0d526173c373e5fd230e9473df47ff3ab570f2c
AJ Jordan <alex@strugee.net>
2020-07-03 22:41:12 -0700

    Export some metadata from __init__.py
    I based this on
    https://github.com/python-attrs/attrs/blob/master/src/attr/__init__.py
    because that person seems to know what they're doing and I don't.

--- amulet_map_editor/__init__.py
+++ amulet_map_editor/__init__.py
@@ -3,6 +3,16 @@ from .util.log import log
 from . import lang
 import os
 
+__description__ = 'A new Minecraft world editor and converter that supports all versions since Java 1.12 and Bedrock 1.7'
+__url__ = "https://www.amulet-editor.com/"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = 'Amulet Team'
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2018-2020 Amulet Team"
+
 _version_path = os.path.join(os.path.dirname(__file__), 'version')
 if os.path.isfile(_version_path):
     with open(_version_path) as _f:
