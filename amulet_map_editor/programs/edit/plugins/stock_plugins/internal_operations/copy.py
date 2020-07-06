e99f66035f3b56b3cba349f20bcea096da75b14e
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-07-06 11:11:50 +0100

    Added an error for copying/cutting empty selections

--- amulet_map_editor/programs/edit/plugins/stock_plugins/internal_operations/copy.py
+++ amulet_map_editor/programs/edit/plugins/stock_plugins/internal_operations/copy.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from amulet.api.structure import structure_cache, Structure
 from amulet.api.data_types import Dimension
 from amulet.api.selection import SelectionGroup
-from amulet_map_editor.programs.edit.plugins.api.errors import OperationSilentAbort
+from amulet_map_editor.programs.edit.plugins.api.errors import OperationSilentAbort, OperationError
 
 if TYPE_CHECKING:
     from amulet.api.world import World
@@ -14,8 +14,11 @@ def copy(
     dimension: Dimension,
     selection: SelectionGroup
 ):
-    structure = Structure.from_world(
-        world, selection, dimension
-    )
-    structure_cache.add_structure(structure)
-    raise OperationSilentAbort
+    if selection:
+        structure = Structure.from_world(
+            world, selection, dimension
+        )
+        structure_cache.add_structure(structure)
+        raise OperationSilentAbort
+    else:
+        raise OperationError("At least one selection is required for the copy operation.")
