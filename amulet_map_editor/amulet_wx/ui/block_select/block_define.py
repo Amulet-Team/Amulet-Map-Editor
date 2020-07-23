4a1c2c6943dc907314480f54b61af4b93b72fb9e
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-07-23 18:30:00 +0100

    Added a property selection UI
    And hooked it up to the full pick block UI
    Still needs a little more work

--- amulet_map_editor/amulet_wx/ui/block_select/block_define.py
+++ amulet_map_editor/amulet_wx/ui/block_select/block_define.py
@@ -5,25 +5,34 @@ import PyMCTranslate
 import amulet_nbt
 from amulet.api.block import PropertyType
 
-from amulet_map_editor.amulet_wx.ui.version_select import VersionSelect, EVT_VERSION_CHANGE
-from amulet_map_editor.amulet_wx.ui.block_select.block import BlockSelect
+from amulet_map_editor.amulet_wx.ui.version_select import (
+    VersionSelect,
+    EVT_VERSION_CHANGE,
+)
+from amulet_map_editor.amulet_wx.ui.block_select.block import (
+    BlockSelect,
+    EVT_BLOCK_CHANGE,
+)
+
+if __name__ != "__main__":
+    from amulet_map_editor.amulet_wx.ui.block_select.properties import PropertySelect
 
 
 class BlockDefine(wx.BoxSizer):
     def __init__(
-            self,
-            parent,
-            translation_manager: PyMCTranslate.TranslationManager,
-            orientation=wx.VERTICAL,
-            platform: str = None,
-            version_number: Tuple[int, int, int] = None,
-            force_blockstate: bool = None,
-            namespace: str = None,
-            block_name: str = None,
-            properties: PropertyType = None,
-            nbt: amulet_nbt.TAG_Compound = None,
-            show_nbt: bool = True,
-            **kwargs
+        self,
+        parent,
+        translation_manager: PyMCTranslate.TranslationManager,
+        orientation=wx.VERTICAL,
+        platform: str = None,
+        version_number: Tuple[int, int, int] = None,
+        force_blockstate: bool = None,
+        namespace: str = None,
+        block_name: str = None,
+        properties: PropertyType = None,
+        nbt: amulet_nbt.TAG_Compound = None,
+        show_nbt: bool = True,
+        **kwargs
     ):
         super().__init__(orientation)
         left_sizer = wx.BoxSizer(wx.VERTICAL)
@@ -43,7 +52,6 @@ class BlockDefine(wx.BoxSizer):
             **kwargs
         )
         left_sizer.Add(self._version_picker, 0, wx.EXPAND)
-        self._version_picker.Bind(EVT_VERSION_CHANGE, self._on_version_change)
 
         self._block_picker = BlockSelect(
             parent,
@@ -56,30 +64,52 @@ class BlockDefine(wx.BoxSizer):
             **kwargs
         )
         left_sizer.Add(self._block_picker, 1, wx.EXPAND | wx.TOP, 5)
+        self._version_picker.Bind(EVT_VERSION_CHANGE, self._on_version_change)
+
+        self._property_picker = PropertySelect(
+            parent,
+            translation_manager,
+            self._version_picker.platform,
+            self._version_picker.version_number,
+            self._version_picker.force_blockstate,
+            self._block_picker.namespace,
+            self._block_picker.block_name,
+            properties,
+        )
+        right_sizer.Add(self._property_picker, 1, wx.EXPAND)
+        self._block_picker.Bind(EVT_BLOCK_CHANGE, self._on_block_change)
 
     def _on_version_change(self, evt):
-        self._block_picker.version = evt.platform, evt.version_number, evt.force_blockstate
+        self._block_picker.version = (
+            evt.platform,
+            evt.version_number,
+            evt.force_blockstate,
+        )
+        evt.Skip()
+
+    def _on_block_change(self, evt):
+        self._property_picker.version_block = (
+            self._version_picker.platform,
+            self._version_picker.version_number,
+            self._version_picker.force_blockstate,
+            evt.namespace,
+            evt.block_name,
+        )
         evt.Skip()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app = wx.App()
+    from amulet_map_editor.amulet_wx.ui.block_select.properties import PropertySelect
+
     def main():
         translation_manager = PyMCTranslate.new_translation_manager()
-        app = wx.App()
         dialog = wx.Dialog(None)
         sizer = wx.BoxSizer()
         dialog.SetSizer(sizer)
-        sizer.Add(
-            BlockDefine(
-                dialog,
-                translation_manager,
-                wx.HORIZONTAL
-            ),
-            1,
-            wx.ALL,
-            5
-        )
+        sizer.Add(BlockDefine(dialog, translation_manager, wx.HORIZONTAL), 1, wx.ALL, 5)
         dialog.Show()
         dialog.Fit()
         app.MainLoop()
+
     main()
