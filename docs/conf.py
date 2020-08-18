09e31597491072aa938bd84fed8eab8f3fb6a8bf
Podshot <ben.gothard3@gmail.com>
2020-08-18 19:35:42 -0400

    Added master_doc field

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Amulet-Map-Editor'
-copyright = '2020, The Amulet Map Editor Team'
-author = 'The Amulet Map Editor Team'
+project = "Amulet-Map-Editor"
+copyright = "2020, The Amulet Map Editor Team"
+author = "The Amulet Map Editor Team"
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.0.1'
+release = "0.7.0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +30,17 @@ release = '0.7.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+master_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +48,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
