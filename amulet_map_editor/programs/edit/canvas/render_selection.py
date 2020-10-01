81330b455e48bd1cd048c272b7fc4023791db691
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-10-01 15:17:37 +0100

    Added a history manager for the selection

--- amulet_map_editor/programs/edit/canvas/render_selection.py
+++ amulet_map_editor/programs/edit/canvas/render_selection.py
@@ -1,23 +1,27 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, Any
 import wx
 import weakref
 
 from amulet_map_editor.api.opengl.mesh.selection import RenderSelectionGroupEditable
 from .events import BoxChangeEvent, BoxEditToggleEvent
 from amulet_map_editor.api.opengl.resource_pack import OpenGLResourcePack
+from amulet.api.history.history_manager import ObjectHistoryManager
+from amulet.api.history import Changeable
+
 
 if TYPE_CHECKING:
     from .edit_canvas import EditCanvas
 
 
-class EditProgramRenderSelectionGroup(RenderSelectionGroupEditable):
+class EditProgramRenderSelectionGroup(RenderSelectionGroupEditable, Changeable):
     def __init__(
         self,
         canvas: "EditCanvas",
         context_identifier: str,
         resource_pack: OpenGLResourcePack,
     ):
-        super().__init__(context_identifier, resource_pack)
+        RenderSelectionGroupEditable.__init__(self, context_identifier, resource_pack)
+        Changeable.__init__(self)
         self._canvas = weakref.ref(canvas)
 
     @property
@@ -29,8 +33,24 @@ class EditProgramRenderSelectionGroup(RenderSelectionGroupEditable):
 
     def _post_box_enable_inputs_event(self):
         super()._post_box_enable_inputs_event()
+        self.changed = True
         wx.PostEvent(self.canvas, BoxEditToggleEvent(edit=False))
 
     def _post_box_disable_inputs_event(self):
         super()._post_box_disable_inputs_event()
         wx.PostEvent(self.canvas, BoxEditToggleEvent(edit=True))
+
+
+class RenderSelectionHistoryManager(ObjectHistoryManager):
+    @property
+    def value(self) -> EditProgramRenderSelectionGroup:
+        return self._value
+
+    def _unpack(self):
+        self.value.set_state_no_event(*self._revision_manager.get_current_entry())
+
+    def _pack_value(self, value: EditProgramRenderSelectionGroup) -> Optional[Any]:
+        return (
+            value.all_selection_corners,
+            value.active_box_index
+        )
