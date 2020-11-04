48dd9748e7e50bdfae61c367e2a9f0bc45aeb953
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-11-04 15:08:39 +0000

    Made the copy operation a generator so the loading bar works

--- amulet_map_editor/programs/edit/plugins/stock_plugins/internal_operations/copy.py
+++ amulet_map_editor/programs/edit/plugins/stock_plugins/internal_operations/copy.py
@@ -7,14 +7,18 @@ from amulet_map_editor.programs.edit.plugins.api.errors import (
     OperationSilentAbort,
     OperationError,
 )
+from amulet.api.data_types import OperationReturnType
 
 if TYPE_CHECKING:
     from amulet.api.level import World
 
 
-def copy(world: "World", dimension: Dimension, selection: SelectionGroup):
+def copy(
+    world: "World", dimension: Dimension, selection: SelectionGroup
+) -> OperationReturnType:
     if selection:
-        structure = world.extract_structure(selection, dimension)
+        yield 0, "Copying"
+        structure = yield from world.extract_structure_iter(selection, dimension)
         structure_cache.add_structure(structure, structure.dimensions[0])
         raise OperationSilentAbort
     else:
