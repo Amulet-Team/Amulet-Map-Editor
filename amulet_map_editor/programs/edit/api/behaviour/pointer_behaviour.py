7bced0be5326a637d69d82d8ac9e2c65c1f9b55c
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-01-26 14:50:34 +0000

    Added some pointer behaviour

--- amulet_map_editor/programs/edit/api/behaviour/pointer_behaviour.py
+++ amulet_map_editor/programs/edit/api/behaviour/pointer_behaviour.py
@@ -1,5 +1,13 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Tuple, Optional
+import numpy
+import wx
+
+from amulet.api.data_types import PointCoordinatesAny
+
+from amulet_map_editor.api.opengl.mesh.selection.render_selection import RenderSelection
+
 from .raycast_behaviour import RaycastBehaviour
+from ..events import EVT_PRE_DRAW, EVT_CAMERA_MOVED
 
 if TYPE_CHECKING:
     from amulet_map_editor.programs.edit.api.canvas import EditCanvas
@@ -7,5 +15,45 @@ if TYPE_CHECKING:
 
 class PointerBehaviour(RaycastBehaviour):
     """Adds the behaviour of the selection pointer."""
+
     def __init__(self, canvas: "EditCanvas"):
         super().__init__(canvas)
+
+        # has the pointer moved
+        self._pointer_moved = True
+
+        # the distance between the camera and the pointer
+        self._pointer_distance = 10
+
+        # the pointer
+        self._pointer: Optional[RenderSelection] = None
+
+    def set_up_events(self):
+        super().set_up_events()
+        if self._pointer is None:
+            self._pointer = RenderSelection(
+                self.canvas.context_identifier,
+                self.canvas.renderer.opengl_resource_pack,
+            )
+        self.canvas.Bind(EVT_PRE_DRAW, self._move_pointer)
+        self.canvas.Bind(EVT_CAMERA_MOVED, self._invalidate_pointer)
+        self.canvas.Bind(wx.EVT_MOTION, self._invalidate_pointer)
+
+    def _invalidate_pointer(self, evt):
+        self._pointer_moved = True
+        evt.Skip()
+
+    def _move_pointer(self, evt):
+        if self._pointer_moved:
+            self._pointer.point1, self._pointer.point2 = self._get_pointer_location()
+            self._pointer_moved = False
+        evt.Skip()
+
+    def _get_pointer_location(self) -> Tuple[PointCoordinatesAny, PointCoordinatesAny]:
+        location = numpy.array(self.box_location_closest(0)[0])
+        return location, location + 1
+
+    def draw(self):
+        self._pointer.draw(
+            self.canvas.camera.transformation_matrix, self.canvas.camera.location
+        )
