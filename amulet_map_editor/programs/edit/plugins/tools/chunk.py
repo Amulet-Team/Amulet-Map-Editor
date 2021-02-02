102cb8dfe78213255335a5e4f86babe972b41832
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-02-02 12:13:50 +0000

    Added an operation to prune chunks

--- amulet_map_editor/programs/edit/plugins/tools/chunk.py
+++ amulet_map_editor/programs/edit/plugins/tools/chunk.py
@@ -4,6 +4,7 @@ import wx
 from amulet_map_editor.api.opengl.camera import Projection
 from amulet_map_editor.programs.edit.api.ui.tool import CameraToolUI
 from amulet.operations.delete_chunk import delete_chunk
+from amulet_map_editor.programs.edit.plugins.operations.stock_plugins.internal_operations.prune_chunks import prune_chunks
 
 if TYPE_CHECKING:
     from amulet_map_editor.programs.edit.api.canvas import EditCanvas
@@ -22,6 +23,10 @@ class ChunkTool(wx.BoxSizer, CameraToolUI):
         button_sizer.Add(delete_button, 0, wx.ALL | wx.EXPAND, 5)
         delete_button.Bind(wx.EVT_BUTTON, self._delete_chunks)
 
+        prune_button = wx.Button(self._button_panel, label="Prune Chunks")
+        button_sizer.Add(prune_button, 0, wx.ALL | wx.EXPAND, 5)
+        prune_button.Bind(wx.EVT_BUTTON, self._prune_chunks)
+
         self.Add(self._button_panel, 0, wx.ALIGN_CENTER_VERTICAL)
 
     @property
@@ -38,4 +43,9 @@ class ChunkTool(wx.BoxSizer, CameraToolUI):
     def _delete_chunks(self, evt):
         self.canvas.run_operation(
             lambda: delete_chunk(self.canvas.world, self.canvas.dimension, self.canvas.selection.selection_group)
+        )
+
+    def _prune_chunks(self, evt):
+        self.canvas.run_operation(
+            lambda: prune_chunks(self.canvas.world, self.canvas.dimension, self.canvas.selection.selection_group)
         )
