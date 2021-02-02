5900ba2baa1df99433b50287c70d591a368211c2
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-02-02 12:13:29 +0000

    Added a chunk UI and moved the delete chunk operation

--- amulet_map_editor/programs/edit/plugins/tools/chunk.py
+++ amulet_map_editor/programs/edit/plugins/tools/chunk.py
@@ -3,6 +3,7 @@ import wx
 
 from amulet_map_editor.api.opengl.camera import Projection
 from amulet_map_editor.programs.edit.api.ui.tool import CameraToolUI
+from amulet.operations.delete_chunk import delete_chunk
 
 if TYPE_CHECKING:
     from amulet_map_editor.programs.edit.api.canvas import EditCanvas
@@ -12,6 +13,16 @@ class ChunkTool(wx.BoxSizer, CameraToolUI):
     def __init__(self, canvas: "EditCanvas"):
         wx.BoxSizer.__init__(self, wx.HORIZONTAL)
         CameraToolUI.__init__(self, canvas)
+
+        self._button_panel = wx.Panel(canvas)
+        button_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._button_panel.SetSizer(button_sizer)
+
+        delete_button = wx.Button(self._button_panel, label="Delete Chunks")
+        button_sizer.Add(delete_button, 0, wx.ALL | wx.EXPAND, 5)
+        delete_button.Bind(wx.EVT_BUTTON, self._delete_chunks)
+
+        self.Add(self._button_panel, 0, wx.ALIGN_CENTER_VERTICAL)
 
     @property
     def name(self) -> str:
@@ -23,3 +34,8 @@ class ChunkTool(wx.BoxSizer, CameraToolUI):
 
     def disable(self):
         super().disable()
+
+    def _delete_chunks(self, evt):
+        self.canvas.run_operation(
+            lambda: delete_chunk(self.canvas.world, self.canvas.dimension, self.canvas.selection.selection_group)
+        )
