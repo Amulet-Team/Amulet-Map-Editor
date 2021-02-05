eb25f8c3e4f041682b0fcc5249eb3ec6ede4084c
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-02-05 17:37:35 +0000

    Added methods to the highlightable group to set the highlighting

--- amulet_map_editor/api/opengl/mesh/selection/group/render_selection_group_highlightable.py
+++ amulet_map_editor/api/opengl/mesh/selection/group/render_selection_group_highlightable.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+import numpy
 from .render_selection_group import (
     RenderSelectionGroup,
 )
@@ -11,3 +13,14 @@ class RenderSelectionGroupHighlightable(RenderSelectionGroup):
         return RenderSelectionHighlightable(
             self._context_identifier, self.resource_pack
         )
+
+    def reset_highlight_edges(self):
+        self._boxes: List[RenderSelectionHighlightable]
+        for box in self._boxes:
+            box.reset_highlight_edges()
+
+    def set_highlight_edges(
+        self, box_index: int, highlight_edges: Union[numpy.ndarray, bool]
+    ):
+        self._boxes: List[RenderSelectionHighlightable]
+        self._boxes[box_index].set_highlight_edges(highlight_edges)
