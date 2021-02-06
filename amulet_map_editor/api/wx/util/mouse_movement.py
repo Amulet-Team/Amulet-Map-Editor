07b2b5a806224c9f67126b2307bb9f8bf2afb42d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-02-06 16:41:28 +0000

    Made mouse movement logic relative
    Previously the mouse location was stored in absolute pixels which caused issues when resizing.
    This makes the stored value relative and converts in and out of absolute as required.

--- amulet_map_editor/api/wx/util/mouse_movement.py
+++ amulet_map_editor/api/wx/util/mouse_movement.py
@@ -9,26 +9,36 @@ class MouseMovement(WindowContainer):
     def __init__(self, window: wx.Window):
         super().__init__(window)
         # the current mouse position
-        self._x = 0
-        self._y = 0
+        self._x: float = 0
+        self._y: float = 0
 
         # the resting position of the mouse from which to calculate the delta
         # if the mouse is warped this will become the warped point
-        self._start_x = 0
-        self._start_y = 0
+        self._start_x: float = 0
+        self._start_y: float = 0
 
         # the sum of all the deltas since it was last reset.
         # when the mouse is warped the delta before it was warped will be added to this value
-        self._delta_x = 0
-        self._delta_y = 0
+        self._delta_x: float = 0
+        self._delta_y: float = 0
 
     def bind_events(self):
         """Set up all events required to run."""
         self.window.Bind(wx.EVT_MOTION, self._on_mouse_motion)
 
+    def _to_relative(self, x: int, y: int) -> Tuple[float, float]:
+        """Convert the x and y values to relative values 0 to 1"""
+        dx, dy = self.window.GetSize()
+        return x / dx, y / dy
+
+    def _to_absolute(self, x: float, y: float) -> Tuple[int, int]:
+        """Convert the relative values to absolute values."""
+        dx, dy = self.window.GetSize()
+        return int(x * dx), int(y * dy)
+
     def _on_mouse_motion(self, evt):
         """Event fired when the mouse is moved."""
-        self._x, self._y = evt.GetPosition()
+        self._x, self._y = self._to_relative(*evt.GetPosition())
         evt.Skip()
 
     def _screen_middle(self) -> Tuple[int, int]:
@@ -43,13 +53,21 @@ class MouseMovement(WindowContainer):
         """Warp the cursor to the middle of the screen."""
         self.xy = self._screen_middle()
 
+    def set_middle(self):
+        """Set the start point to the middle of the screen. Does not warp."""
+        self._start_x, self._start_y = 0.5, 0.5
+
     def _warp(self):
-        self.window.WarpPointer(self._x, self._y)
+        self.window.WarpPointer(
+            *self._to_absolute(
+                self._x, self._y
+            )
+        )
 
     @property
     def x(self) -> int:
         """The x pixel location of the mouse in the parent window."""
-        return self._x
+        return self._to_absolute(self._x, 0)[0]
 
     @x.setter
     def x(self, x: int):
@@ -57,13 +75,13 @@ class MouseMovement(WindowContainer):
         Will warp the cursor to this position and create a mouse move event."""
         assert type(x) is int, "x must be an int"
         self._delta_x += self._x - self._start_x
-        self._start_x = self._x = x
+        self._start_x = self._x = self._to_relative(x, 0)[0]
         self._warp()
 
     @property
     def y(self) -> int:
         """The y pixel location of the mouse in the parent window."""
-        return self._y
+        return self._to_absolute(0, self._y)[1]
 
     @y.setter
     def y(self, y: int):
@@ -71,13 +89,13 @@ class MouseMovement(WindowContainer):
         Will warp the cursor to this position and create a mouse move event."""
         assert type(y) is int, "y must be an int"
         self._delta_y += self._y - self._start_y
-        self._start_y = self._y = y
+        self._start_y = self._y = self._to_relative(0, y)[1]
         self._warp()
 
     @property
     def xy(self) -> Tuple[int, int]:
         """The x and y pixel location of the mouse in the parent window."""
-        return self._x, self._y
+        return self._to_absolute(self._x, self._y)
 
     @xy.setter
     def xy(self, xy: Tuple[int, int]):
@@ -89,8 +107,7 @@ class MouseMovement(WindowContainer):
         x, y = xy
         self._delta_x += self._x - self._start_x
         self._delta_y += self._y - self._start_y
-        self._start_x = self._x = x
-        self._start_y = self._y = y
+        (self._start_x, self._start_y) = (self._x, self._y) = self._to_relative(x, y)
         self._warp()
 
     def reset_delta(self):
@@ -106,16 +123,19 @@ class MouseMovement(WindowContainer):
     def delta_x(self) -> int:
         """The x pixel distance between the current location and the location when reset_delta was called.
         If the pointer was warped the offset before it was warped will be added to this."""
-        return self._x - self._start_x + self._delta_x
+        return self.delta_xy[0]
 
     @property
     def delta_y(self) -> int:
         """The y pixel distance between the current location and the location when reset_delta was called.
         If the pointer was warped the offset before it was warped will be added to this."""
-        return self._y - self._start_y + self._delta_y
+        return self.delta_xy[1]
 
     @property
     def delta_xy(self) -> Tuple[int, int]:
         """The x and y pixel distance between the current location and the location when reset_delta was called.
         If the pointer was warped the offset before it was warped will be added to this."""
-        return self.delta_x, self.delta_y
+        return self._to_absolute(
+            self._x - self._start_x + self._delta_x,
+            self._y - self._start_y + self._delta_y
+        )
