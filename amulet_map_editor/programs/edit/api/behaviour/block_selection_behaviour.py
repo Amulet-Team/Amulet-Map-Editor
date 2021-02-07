e884275a75c95fb7ee54bc80be32a3b7e22fbbfe
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-02-07 11:12:58 +0000

    Reimplemented box resizing and escaping

--- amulet_map_editor/programs/edit/api/behaviour/block_selection_behaviour.py
+++ amulet_map_editor/programs/edit/api/behaviour/block_selection_behaviour.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING, Tuple, Optional
 import time
 import numpy
+import wx
 
 from ..events import (
     InputPressEvent,
@@ -39,14 +40,30 @@ class BlockSelectionBehaviour(PointerBehaviour):
         self._active_selection: Optional[RenderSelectionEditable] = None
         self._editing = False  # is the active selection being created or resized
         self._press_time = 0  # the time when the last box edit started
-        self._start_box = numpy.zeros((2, 3))
-        self._highlight = False
+        self._start_point_1 = numpy.zeros((2, 3))  # the state of the cursor when editing starts
+        self._start_point_2 = numpy.zeros((2, 3))  # the state of the cursor when editing starts
+        self._highlight = False  # is a box being highlighted
+        self._initial_box: Optional[numpy.ndarray] = None  # the state of the box when editing started
+        self._pointer_mask = numpy.zeros((2, 3), dtype=numpy.bool)
+
+    def _load_active_selection(self):
+        if self._active_selection is None:
+            self._active_selection = RenderSelectionEditable(
+                self.canvas.context_identifier,
+                self.canvas.renderer.opengl_resource_pack,
+            )
+
+    def _unload_active_selection(self):
+        if self._active_selection is not None:
+            self._active_selection.unload()
+            self._active_selection = None
 
     def bind_events(self):
         super().bind_events()
         self.canvas.Bind(EVT_INPUT_PRESS, self._on_input_press)
         self.canvas.Bind(EVT_INPUT_RELEASE, self._on_input_release)
         self.canvas.Bind(EVT_SELECTION_CHANGE, self._on_selection_change)
+        self.canvas.Bind(wx.EVT_KEY_DOWN, self._on_key_press)
 
     def enable(self):
         self._pull_selection()
@@ -66,45 +83,122 @@ class BlockSelectionBehaviour(PointerBehaviour):
     def _on_input_press(self, evt: InputPressEvent):
         if evt.action_id == "box click":
             if not self._editing:
-                self._press_time = time.time()
-                self._editing = True
+                default_create = True
 
-                if self._active_selection is None:
-                    self._active_selection = RenderSelectionEditable(
-                        self.canvas.context_identifier,
-                        self.canvas.renderer.opengl_resource_pack,
-                    )
-                self._start_box = self._pointer.bounds
-                # self._selection.box_select_toggle(
-                #     "add box modifier" in self.canvas.buttons.pressed_actions
-                # )
+                if "add box modifier" in self.canvas.buttons.pressed_actions:
+                    # create a new box
+                    if self._active_selection is not None:
+                        # move the existing active to the inactive
+                        self._selection.selection_group = self._selection.selection_group + self._active_selection.selection_group
+
+                elif self._highlight:
+                    # edit the highlighted box
+                    box_index, faces = self._get_box_faces()
+                    if box_index is None:
+                        # this shouldn't happen but just to be safe.
+                        self._selection.selection_group = SelectionGroup()
+                    else:
+                        if box_index == len(self._selection):
+                            # the active selection is highlighted
+                            self._press_time = time.time()
+                            self._editing = True
+                            self._start_point_1, self._start_point_2 = self._get_active_points()
+                            self._active_selection.set_highlight_edges(faces)
+                            self._pointer_mask = faces
+                            self._active_selection.locked = False
+                            self._initial_box = None
+                        else:
+                            # a static selection box is highlighted
+                            selection_group = self.selection_group
+                            self._selection.selection_group = selection_group[:box_index] + selection_group[box_index+1:]
+                            self._active_selection.selection_box = selection_group[box_index]
+
+                        default_create = False
+                        self._initial_box = self._active_selection.points
+                else:
+                    # clear the selection
+                    self._selection.selection_group = SelectionGroup()
+
+                self._load_active_selection()
+                if default_create:
+                    self._press_time = time.time()
+                    self._editing = True
+                    self._start_point_1 = self._pointer.bounds
+                    self._pointer_mask = numpy.array([[False]*3, [True]*3], dtype=numpy.bool)
+                    self._active_selection.set_highlight_edges(self._pointer_mask)
+                    self._active_selection.locked = False
+                    self._initial_box = None
+
         elif evt.action_id == "deselect boxes":
             self.selection_group = SelectionGroup()
             self._push_selection()
         elif evt.action_id == "remove box":
             if "deselect boxes" not in self.canvas.buttons.pressed_actions:
-                self.selection_group = SelectionGroup()
+                self.selection_group = self.selection_group[:-1]
                 self._push_selection()
         evt.Skip()
+
+    def _on_key_press(self, evt: wx.KeyEvent):
+        key = evt.GetUnicodeKey() or evt.GetKeyCode()
+        if key == wx.WXK_ESCAPE:
+            self._escape()
+        evt.Skip()
+
+    def _escape(self):
+        """Reset the state to how it was before editing."""
+        if self._editing:
+            if self._initial_box is None:
+                # there was no initial box
+                if self._selection:
+                    # static selection exists. Load the last into the active.
+                    self._load_active_selection()
+                    selection_group = self.selection_group
+                    self._selection.selection_group = selection_group[:-1]
+                    self._active_selection.selection_box = selection_group[-1]
+                    self._active_selection.locked = True
+                else:
+                    self._unload_active_selection()
+            else:
+                # there was an initial box
+                self._load_active_selection()
+                self._active_selection.points = self._initial_box
+                self._active_selection.locked = True
+            self._editing = False
+
+    def _get_active_points(self):
+        """Get the 1x1x1 box coords for the active selection."""
+        p1, p2 = numpy.zeros((2, 3)), numpy.zeros((2, 3))
+        if self._active_selection is not None:
+            p1[0], p2[0] = self._active_selection.points
+            mult = (p1[0] < p2[0]) * 2 - 1
+            p1[1] = p1[0] + 1 * mult
+            p2[1] = p2[0] - 1 * mult
+        return p1, p2
 
     def _get_editing_selection(self) -> Tuple[numpy.ndarray, numpy.ndarray]:
         """Get the minimum and maximum points of the editing selection.
         This is based on the stored starting pointer and the current pointer."""
-        start_1, start_2 = self._start_box.copy()
-        point_1, point_2 = self._pointer.bounds.copy()
+        start_point_1 = self._start_point_1.copy()
+        start_point_2 = self._start_point_2.copy()
+        pointer = self._pointer.bounds.copy()
+        start_point_1[:, self._pointer_mask[0]] = pointer[:, self._pointer_mask[0]]
+        start_point_2[:, self._pointer_mask[1]] = pointer[:, self._pointer_mask[1]]
+        start_1, start_2 = start_point_1
+        point_1, point_2 = start_point_2
         start = start_1
         point = point_1
-        mask = numpy.abs(point - start_1) < numpy.abs(point - start_2)
-        start[mask] = start_2[mask]
         mask = numpy.abs(start - point_1) < numpy.abs(start - point_2)
         point[mask] = point_2[mask]
+        mask = numpy.abs(point - start_1) < numpy.abs(point - start_2)
+        start[mask] = start_2[mask]
         return start, point
 
     def _on_input_release(self, evt: InputReleaseEvent):
         if evt.action_id == "box click":
-            if time.time() - self._press_time > 0.1:
+            if self._editing and time.time() - self._press_time > 0.1:
                 self._editing = False
                 self._active_selection.locked = True
+                self._push_selection()
         evt.Skip()
 
     def _move_pointer(self, evt):
@@ -143,8 +237,7 @@ class BlockSelectionBehaviour(PointerBehaviour):
         if len(selection_group) == 0:
             # unload the active selection
             if self._active_selection is not None:
-                self._active_selection.unload()
-                self._active_selection = None
+                self._unload_active_selection()
 
         if len(selection_group) <= 1:
             # unload the selection group
@@ -152,16 +245,12 @@ class BlockSelectionBehaviour(PointerBehaviour):
 
         if len(selection_group) >= 1:
             # load the active selection
-            if self._active_selection is None:
-                self._active_selection = RenderSelectionEditable(
-                    self.canvas.context_identifier,
-                    self.canvas.renderer.opengl_resource_pack,
-                )
+            self._load_active_selection()
             self._active_selection.selection_box = selection_group[-1]
 
         if len(selection_group) >= 2:
             # load the selection group
-            self._selection.selection_group = SelectionGroup(selection_group[:-1])
+            self._selection.selection_group = selection_group[:-1]
 
     def _get_box_faces(self) -> Tuple[Optional[int], numpy.ndarray]:
         """Get a bool array of which faces the look vector hits.
