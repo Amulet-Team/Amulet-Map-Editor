90267b00fc9e995bde9b5c1742c840b7e47616c6
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-03-25 09:55:12 +0000

    Added copy and paste logic to goto ui

--- amulet_map_editor/programs/edit/api/ui/goto.py
+++ amulet_map_editor/programs/edit/api/ui/goto.py
@@ -1,7 +1,13 @@
 from typing import Optional, Tuple
 import wx
+import re
+
 from amulet_map_editor.api.wx.ui.simple import SimpleDialog
 from amulet.api.data_types import PointCoordinates
+
+CoordRegex = re.compile(
+    r"^\s*(?P<x>-?[0-9]+\.?[0-9]*),?\s*(?P<y>-?[0-9]+\.?[0-9]*),?\s*(?P<z>-?[0-9]+\.?[0-9]*),?\s*$"
+)
 
 
 def show_goto(
@@ -31,9 +37,44 @@ class GoTo(SimpleDialog):
         self.sizer.Add(self.y, 1, wx.CENTER | wx.ALL, 5)
         self.sizer.Add(z_text, 0, wx.CENTER | wx.ALL, 5)
         self.sizer.Add(self.z, 1, wx.CENTER | wx.ALL, 5)
+        self.x.Bind(wx.EVT_CHAR, self._on_text)
+        self.y.Bind(wx.EVT_CHAR, self._on_text)
+        self.z.Bind(wx.EVT_CHAR, self._on_text)
         self.Fit()
 
     @property
     def location(self) -> PointCoordinates:
         return self.x.GetValue(), self.y.GetValue(), self.z.GetValue()
 
+    def _on_text(self, evt):
+        if evt.ControlDown() and evt.GetKeyCode() == 3:
+            # Ctrl+C
+            if wx.TheClipboard.Open():
+                wx.TheClipboard.SetData(
+                    wx.TextDataObject(
+                        "{} {} {}".format(
+                            round(self.x.GetValue(), 5),
+                            round(self.y.GetValue(), 5),
+                            round(self.z.GetValue(), 5),
+                        )
+                    )
+                )
+                wx.TheClipboard.Close()
+        elif evt.ControlDown() and evt.GetKeyCode() == 22:
+            # Ctrl+V
+            text = ""
+            text_data = wx.TextDataObject()
+            if wx.TheClipboard.Open():
+                success = wx.TheClipboard.GetData(text_data)
+                wx.TheClipboard.Close()
+                if success:
+                    text = text_data.GetText()
+            match = CoordRegex.fullmatch(text)
+            if match:
+                self.x.SetValue(float(match.group("x")))
+                self.y.SetValue(float(match.group("y")))
+                self.z.SetValue(float(match.group("z")))
+            else:
+                evt.Skip()
+        else:
+            evt.Skip()
