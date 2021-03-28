7d87f5aa23decafbcc09d29da48fac3e15e0d5ca
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-03-28 16:59:04 +0100

    Added culling in 2D mode in the chunk tool

--- amulet_map_editor/programs/edit/plugins/tools/chunk.py
+++ amulet_map_editor/programs/edit/plugins/tools/chunk.py
@@ -1,11 +1,14 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 import wx
 from OpenGL.GL import (
     glClear,
     GL_DEPTH_BUFFER_BIT,
+    GL_DEPTH_TEST,
+    glEnable,
+    glDisable,
 )
 
-from amulet_map_editor.api.opengl.camera import Projection
+from amulet_map_editor.api.opengl.camera import Projection, EVT_CAMERA_MOVED
 from amulet_map_editor.programs.edit.api.ui.tool import DefaultBaseToolUI
 from amulet_map_editor.programs.edit.api.behaviour import ChunkSelectionBehaviour
 from amulet.operations.delete_chunk import delete_chunk
@@ -28,6 +31,37 @@ class ChunkTool(wx.BoxSizer, DefaultBaseToolUI):
         button_sizer = wx.BoxSizer(wx.VERTICAL)
         self._button_panel.SetSizer(button_sizer)
 
+        min_range = -30_000_000
+        max_range = 30_000_000
+
+        y_sizer = wx.FlexGridSizer(2, 5, 5)
+        button_sizer.Add(y_sizer, flag=wx.LEFT | wx.TOP | wx.RIGHT, border=5)
+        min_y_label = wx.StaticText(self._button_panel, label="min_y")
+        y_sizer.Add(min_y_label, flag=wx.CENTER | wx.EXPAND)
+        self._min_y = wx.SpinCtrl(
+            self._button_panel,
+            min=min_range,
+            max=max_range,
+            initial=min(
+                max_range, max(min_range, self.canvas.world.selection_bounds.min[1])
+            ),
+        )
+        y_sizer.Add(self._min_y, flag=wx.CENTER | wx.EXPAND)
+        self._min_y.Bind(wx.EVT_SPINCTRL, self._on_update_clipping)
+
+        max_y_label = wx.StaticText(self._button_panel, label="max_y")
+        y_sizer.Add(max_y_label, flag=wx.CENTER | wx.EXPAND)
+        self._max_y = wx.SpinCtrl(
+            self._button_panel,
+            min=min_range,
+            max=max_range,
+            initial=min(
+                max_range, max(min_range, self.canvas.world.selection_bounds.max[1])
+            ),
+        )
+        y_sizer.Add(self._max_y, flag=wx.CENTER | wx.EXPAND)
+        self._max_y.Bind(wx.EVT_SPINCTRL, self._on_update_clipping)
+
         delete_button = wx.Button(self._button_panel, label="Delete Chunks")
         button_sizer.Add(delete_button, 0, wx.ALL | wx.EXPAND, 5)
         delete_button.Bind(wx.EVT_BUTTON, self._delete_chunks)
@@ -45,14 +79,28 @@ class ChunkTool(wx.BoxSizer, DefaultBaseToolUI):
     def bind_events(self):
         super().bind_events()
         self._selection.bind_events()
+        self.canvas.Bind(EVT_CAMERA_MOVED, self._on_update_clipping)
 
     def enable(self):
         self.Layout()
         self.canvas.camera.projection_mode = Projection.TOP_DOWN
         self._selection.enable()
+        self._update_clipping()
 
     def disable(self):
         super().disable()
+        self.canvas.camera.orthographic_clipping = -(10 ** 5), 10 ** 5
+
+    def _on_update_clipping(self, evt):
+        self._update_clipping()
+        evt.Skip()
+
+    def _update_clipping(self):
+        y = self.canvas.camera.location[1]
+        self.canvas.camera.orthographic_clipping = (
+            y - self._max_y.GetValue() - 1,
+            y - self._min_y.GetValue() + 1,
+        )
 
     def _delete_chunks(self, evt):
         self.canvas.run_operation(
@@ -78,5 +126,13 @@ class ChunkTool(wx.BoxSizer, DefaultBaseToolUI):
             self.canvas.renderer.draw_sky_box()
             glClear(GL_DEPTH_BUFFER_BIT)
         self.canvas.renderer.draw_level()
-        self._selection.draw()
+        if self.canvas.camera.projection_mode == Projection.PERSPECTIVE:
+            self._selection.draw()
+        else:
+            glDisable(GL_DEPTH_TEST)
+            clip = self.canvas.camera.orthographic_clipping
+            self.canvas.camera.orthographic_clipping = -(10 ** 5), 10 ** 5
+            self._selection.draw()
+            self.canvas.camera.orthographic_clipping = clip
+            glEnable(GL_DEPTH_TEST)
         self.canvas.renderer.end_draw()
