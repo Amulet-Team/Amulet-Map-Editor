c9beae7314c1eb508317695245f84509eb10fd22
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-03-31 15:49:57 +0100

    Try to fix locale issues

--- amulet_map_editor/api/framework/app.py
+++ amulet_map_editor/api/framework/app.py
@@ -1,18 +1,21 @@
 import wx
 from .amulet_ui import AmuletUI
+from amulet_map_editor import log
 
 
 class AmuletApp(wx.App):
     def __init__(self):
         super().__init__()
         self._frame = AmuletUI(None)
+        self.locale = wx.Locale(wx.LANGUAGE_DEFAULT)
 
-    def InitLocale(self):
-        self.ResetLocale()
-        import locale
-        lang, enc = locale.getdefaultlocale()
-        self._initial_locale = wx.Locale(lang, lang[:2], lang)
-        try:
-            locale.setlocale(locale.LC_ALL, lang)
-        except locale.Error:
-            locale.setlocale(locale.LC_ALL, (lang, enc))
+    # def InitLocale(self):
+    #     # https://discuss.wxpython.org/t/what-is-wxpython-doing-to-the-locale-to-makes-pandas-crash/34606/18
+    #     self.ResetLocale()
+    #     import locale
+    #     lang, enc = locale.getdefaultlocale()
+    #     self._initial_locale = wx.Locale(lang, lang[:2], lang)
+    #     try:
+    #         locale.setlocale(locale.LC_ALL, lang)
+    #     except locale.Error:
+    #         locale.setlocale(locale.LC_ALL, (lang, enc))
