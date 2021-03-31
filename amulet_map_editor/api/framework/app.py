ed7f5c566c65d5075558756ba053ec77082013b3
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-03-31 16:30:47 +0100

    Moved locale to before frame creation

--- amulet_map_editor/api/framework/app.py
+++ amulet_map_editor/api/framework/app.py
@@ -6,8 +6,8 @@ from amulet_map_editor import log
 class AmuletApp(wx.App):
     def __init__(self):
         super().__init__()
-        self._frame = AmuletUI(None)
         self.locale = wx.Locale(wx.LANGUAGE_DEFAULT)
+        self._frame = AmuletUI(None)
 
     # def InitLocale(self):
     #     # https://discuss.wxpython.org/t/what-is-wxpython-doing-to-the-locale-to-makes-pandas-crash/34606/18
