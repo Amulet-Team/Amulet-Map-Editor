ada705c6db6398c533f4d97c5a1c14efd24ac29a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-06-23 11:15:59 +0100

    Fixed an issue with wxpython and linux

--- amulet_map_editor/__main__.py
+++ amulet_map_editor/__main__.py
@@ -5,12 +5,17 @@ import sys
 if sys.version_info[:2] < (3, 7):
     raise Exception("Must be using Python 3.7+")
 
+import os
+import traceback
+import wx.__version__
 from amulet_map_editor import log
 from amulet_map_editor.api.framework import AmuletApp
-import traceback
 
 
 if __name__ == "__main__":
+    if sys.platform == "linux" and wx.__version__.VERSION >= (4, 1, 1):
+        # bug 247
+        os.environ['PYOPENGL_PLATFORM'] = 'egl'
     try:
         app = AmuletApp(0)
         app.MainLoop()
