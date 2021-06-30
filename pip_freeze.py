9e8733d3ad7a48b84f73cbf4bcdaf633df8f29a1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-06-30 12:48:56 +0100

    Freeze the first party requirements before uploading to pypi
    Users installing from source needed to manually update the dependencies which caused issues.
    This will force the latest version of the first party libraries at build time.
    It will also fix some other issues that may present themselves.
    #354

---
+++ pip_freeze.py
@@ -0,0 +1,26 @@
+from pip._internal.operations import freeze
+
+
+def main():
+    with open("requirements.txt") as f:
+        requirements = [r.strip() for r in f.readlines()]
+    first_party = {
+        "amulet-core",
+        "amulet-nbt",
+        "pymctranslate",
+        "minecraft-resource-pack",
+    }
+    installed = {
+        r.split("==")[0].lower(): r for r in freeze.freeze() if "==" in r
+    }
+    for index, r in enumerate(requirements):
+        if r[0] != "#" and "~=" in r:
+            req = r.split("~=")[0]
+            if req in first_party and req in installed:
+                requirements[index] = installed[req]
+    with open("requirements.txt", "w") as f:
+        f.write("\n".join(requirements))
+
+
+if __name__ == '__main__':
+    main()
