3f6bb02fe097fe3ab20b60286a7661b5216a99d3
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-07-24 11:36:57 +0100

    Fixed setting through property rather than method

--- amulet_map_editor/api/wx/ui/mc/base/base_define.py
+++ amulet_map_editor/api/wx/ui/mc/base/base_define.py
@@ -75,14 +75,17 @@ class BaseDefine(wx.Panel, BaseMCVersion):
         raise NotImplementedError
 
     def push(self) -> bool:
+        self.set_platform(self.platform)
+        self.set_version_number(self.version_number)
+        self.set_force_blockstate(self.force_blockstate)
         if (
             self.platform != self._version_picker.platform
             or self.version_number != self._version_picker.version_number
             or self.force_blockstate != self._version_picker.force_blockstate
         ):
-            self._version_picker.platform = self.platform
-            self._version_picker.version_number = self.version_number
-            self._version_picker.force_blockstate = self.force_blockstate
+            self._version_picker.set_platform(self.platform)
+            self._version_picker.set_version_number(self.version_number)
+            self._version_picker.set_force_blockstate(self.force_blockstate)
             self._version_picker.push()
             return True
         return False
