3905d6480c2e67d2e245297a2eaa30c645d11aa2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-07-27 12:22:41 +0100

    Added a state manager for normal and wildcard properties

--- amulet_map_editor/api/wx/ui/mc/base/api/block/normal.py
+++ amulet_map_editor/api/wx/ui/mc/base/api/block/normal.py
@@ -1,9 +1,13 @@
-from typing import Optional, Tuple
+from typing import Optional
 
-from amulet.api.block import PropertyType, Block
-from amulet.api.block_entity import BlockEntity
+import PyMCTranslate
+import amulet_nbt
+from amulet.api.data_types import VersionNumberTuple
+from amulet.api.block import PropertyType, Block, PropertyDataTypes
 
-from .identifier import BaseMCBlockIdentifierAPI
+# from amulet.api.block_entity import BlockEntity
+
+from .identifier import BaseMCBlockIdentifierAPI, BaseMCBlockIdentifier
 
 
 class NormalMCBlockAPI(BaseMCBlockIdentifierAPI):
@@ -23,18 +27,97 @@ class NormalMCBlockAPI(BaseMCBlockIdentifierAPI):
     def block(self, block: Block):
         raise NotImplementedError
 
+
+class NormalMCBlock(BaseMCBlockIdentifier, NormalMCBlockAPI):
+    def __init__(
+        self,
+        translation_manager: PyMCTranslate.TranslationManager,
+        platform: str = None,
+        version_number: VersionNumberTuple = None,
+        force_blockstate: bool = False,
+        namespace: str = None,
+        base_name: str = None,
+        properties: PropertyType = None,
+    ):
+        super().__init__(
+            translation_manager,
+            platform,
+            version_number,
+            force_blockstate,
+            namespace,
+            base_name,
+        )
+        self._properties = None
+        self._set_properties(properties)
+
     @property
-    def block_entity(self) -> Optional[BlockEntity]:
-        raise NotImplementedError
+    def properties(self) -> PropertyType:
+        return self._properties
 
-    @block_entity.setter
-    def block_entity(self, block_entity: Optional[BlockEntity]):
-        raise NotImplementedError
+    @properties.setter
+    def properties(self, properties: PropertyType):
+        self._set_properties(properties)
+        self._schedule_push()
+
+    def _set_properties(self, properties: Optional[PropertyType]):
+        """
+        Set the active properties.
+        Changes will not propagate.
+        :meth:`push` must be called once all desired states are set.
+
+        :param properties: A dictionary mapping the property name to the property value.
+        """
+        self._properties = {}
+        block_manager = self._translation_manager.get_version(
+            self.platform, self.version_number
+        ).block
+        if self.platform in block_manager.namespaces(
+            self.force_blockstate
+        ) and self.base_name in block_manager.base_names(
+            self.namespace, self.force_blockstate
+        ):
+            # Vanilla block. Make sure the property is valid
+            block_spec = block_manager.get_specification(
+                self.namespace, self.base_name, self.force_blockstate
+            )
+            props = block_spec.get("properties", {})
+            defaults = block_spec.get("defaults", {})
+            if isinstance(properties, dict):
+                for name, ps in props.items():
+                    if (
+                        name in properties
+                        and isinstance(properties[name], PropertyDataTypes)
+                        and properties[name].to_snbt() in ps
+                    ):
+                        self._properties[name] = properties[name]
+                    else:
+                        self._properties[name] = amulet_nbt.from_snbt(defaults[name])
+            else:
+                for name, snbt in defaults.items():
+                    self._properties[name] = amulet_nbt.from_snbt(snbt)
+        elif isinstance(properties, dict):
+            for name, nbt in properties.items():
+                if isinstance(nbt, PropertyDataTypes):
+                    self._properties[name] = nbt
 
     @property
-    def universal_block(self) -> Tuple[Block, Optional[BlockEntity]]:
-        raise NotImplementedError
+    def block(self) -> Block:
+        return Block(self.namespace, self.base_name, self.properties)
 
-    @universal_block.setter
-    def universal_block(self, universal_block: Tuple[Block, Optional[BlockEntity]]):
-        raise NotImplementedError
+    @block.setter
+    def block(self, block: Block):
+        self._set_block(block)
+        self._schedule_push()
+
+    def _set_block(self, block: Optional[Block]):
+        """
+        Set the active block.
+        Namespace, base name and properties will be pulled from this.
+        Changes will not propagate.
+        :meth:`push` must be called once all desired states are set.
+
+        :param block: The block to set.
+        """
+        self._set_namespace(block.namespace)
+        self._set_base_name(block.base_name)
+        self._set_properties(block.properties)
--- amulet_map_editor/api/wx/ui/mc/base/api/block/wildcard.py
+++ amulet_map_editor/api/wx/ui/mc/base/api/block/wildcard.py
@@ -1,6 +1,10 @@
-from amulet.api.block import PropertyTypeMultiple
+from typing import Optional
+import PyMCTranslate
+import amulet_nbt
+from amulet.api.data_types import VersionNumberTuple
+from amulet.api.block import PropertyTypeMultiple, PropertyDataTypes
 
-from .identifier import BaseMCBlockIdentifierAPI
+from .identifier import BaseMCBlockIdentifierAPI, BaseMCBlockIdentifier
 
 
 class WildcardMCBlockAPI(BaseMCBlockIdentifierAPI):
@@ -21,3 +25,99 @@ class WildcardMCBlockAPI(BaseMCBlockIdentifierAPI):
     @selected_properties.setter
     def selected_properties(self, properties: PropertyTypeMultiple):
         raise NotImplementedError
+
+
+class WildcardMCBlock(BaseMCBlockIdentifier, WildcardMCBlockAPI):
+    def __init__(
+        self,
+        translation_manager: PyMCTranslate.TranslationManager,
+        platform: str = None,
+        version_number: VersionNumberTuple = None,
+        force_blockstate: bool = False,
+        namespace: str = None,
+        base_name: str = None,
+        selected_properties: PropertyTypeMultiple = None,
+        all_properties: PropertyTypeMultiple = None,
+    ):
+        super().__init__(
+            translation_manager,
+            platform,
+            version_number,
+            force_blockstate,
+            namespace,
+            base_name,
+        )
+        self._all_properties = None
+        self._set_all_properties(all_properties)
+        self._selected_properties = None
+        self._set_selected_properties(selected_properties)
+
+    @property
+    def all_properties(self) -> PropertyTypeMultiple:
+        return self._all_properties
+
+    @all_properties.setter
+    def all_properties(self, all_properties: PropertyTypeMultiple):
+        self._set_all_properties(all_properties)
+        self._schedule_push()
+
+    def _clean_properties(
+        self, properties: Optional[PropertyTypeMultiple]
+    ) -> PropertyTypeMultiple:
+        out_properties: PropertyTypeMultiple = {}
+        block_manager = self._translation_manager.get_version(
+            self.platform, self.version_number
+        ).block
+        if self.platform in block_manager.namespaces(
+            self.force_blockstate
+        ) and self.base_name in block_manager.base_names(
+            self.namespace, self.force_blockstate
+        ):
+            # Vanilla block. Make sure the property is valid
+            block_spec = block_manager.get_specification(
+                self.namespace, self.base_name, self.force_blockstate
+            )
+            props = block_spec.get("properties", {})
+            if isinstance(properties, dict):
+                for name, ps in props.items():
+                    if name in properties:
+                        out_properties[name] = tuple(
+                            nbt
+                            for nbt in properties[name]
+                            if isinstance(nbt, PropertyDataTypes)
+                            and nbt.to_snbt() in ps
+                        )
+            else:
+                for name, snbts in props.items():
+                    out_properties[name] = tuple(
+                        amulet_nbt.from_snbt(snbt) for snbt in snbts
+                    )
+        elif isinstance(properties, dict):
+            for name, nbts in properties.items():
+                out_properties[name] = tuple(
+                    nbt for nbt in nbts if isinstance(nbt, PropertyDataTypes)
+                )
+        return out_properties
+
+    def _set_all_properties(self, all_properties: Optional[PropertyTypeMultiple]):
+        self._all_properties: PropertyTypeMultiple = self._clean_properties(
+            all_properties
+        )
+
+    @property
+    def selected_properties(self) -> PropertyTypeMultiple:
+        return self._selected_properties
+
+    @selected_properties.setter
+    def selected_properties(self, selected_properties: PropertyTypeMultiple):
+        self._set_selected_properties(selected_properties)
+        self._schedule_push()
+
+    def _set_selected_properties(
+        self, selected_properties: Optional[PropertyTypeMultiple]
+    ):
+        self._selected_properties = {
+            name: tuple(nbt for nbt in nbts if nbt in self.all_properties[name])
+            for name, nbts in self._clean_properties(selected_properties).items()
+            if name in self.all_properties
+        }
