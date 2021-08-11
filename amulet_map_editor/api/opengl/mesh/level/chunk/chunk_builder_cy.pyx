21db697e28b844b7f7af74e514e38b8820a81ea2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-08-11 17:07:12 +0100

    Partially hooked up the new storage system to the mesh generator
    There are a number of issues and what I believe to be a stack overflow that I need to debug

--- amulet_map_editor/api/opengl/mesh/level/chunk/chunk_builder_cy.pyx
+++ amulet_map_editor/api/opengl/mesh/level/chunk/chunk_builder_cy.pyx
@@ -41,21 +41,22 @@ CULL_MAP[4][:] = (1, 0, 0)
 CULL_MAP[5][:] = (0, 0, 1)
 CULL_MAP[6][:] = (-1, 0, 0)
 
-DEF ARRAY_SIZE = 10_000
-DEF ATTR_COUNT = 12
+DEF ARRAY_VERT_COUNT = 10_000  # The number of vertices in the table
+DEF ATTR_COUNT = 12  # The number of float attributes per vertex
+DEF ARRAY_SIZE = ARRAY_VERT_COUNT * ATTR_COUNT
 
 
 cdef struct VertArray:
     float* arr  # pointer to the array
-    int size  # the size of the array in bytes
+    int size  # the number of floats in the array
 
 cdef VertArray* vert_array_init(array.array arr):
     assert arr.typecode == "f", "arr must be a float array"
     assert len(arr) and len(arr) % (ATTR_COUNT*3) == 0, "arr must have a multiple of 36 values"
     vert_array = <VertArray*>calloc(1, sizeof(VertArray))
-    vert_array.size = arr.itemsize * len(arr)
-    vert_array.arr = <float*>malloc(vert_array.size)
-    memcpy(vert_array.arr, &arr.data.as_floats[0], vert_array.size)
+    vert_array.size = len(arr)
+    vert_array.arr = <float*>malloc(vert_array.size * sizeof(float))
+    memcpy(vert_array.arr, &arr.data.as_floats[0], vert_array.size * sizeof(float))
     return vert_array
 
 cdef void vert_array_free(VertArray* vert_array):
@@ -65,12 +66,17 @@ cdef void vert_array_free(VertArray* vert_array):
 
 cdef struct BlockModel:
     VertArray* faces[7]
+    char is_transparent
 
-cdef BlockModel* block_model_init(dict face_data):
+cdef BlockModel* block_model_init(dict face_data, char is_transparent):
     block_model = <BlockModel*>calloc(1, sizeof(BlockModel))
+    block_model.is_transparent = is_transparent
     for cull_id, index in CULL_STR_INDEX.items():
         if cull_id in face_data:
             arr = face_data[cull_id]
+            print("arr")
+            if isinstance(arr, numpy.ndarray):
+                arr = array.array("f", arr.ravel())
             if isinstance(arr, array.array) and arr.typecode == "f":
                 block_model.faces[index] = vert_array_init(arr)
         else:
@@ -111,28 +117,34 @@ cdef class BlockModelManager:
             free(self.blocks)
             self.blocks = blocks_temp
 
-    cpdef add_block(self, dict face_data):
+    cpdef add_block(self, dict face_data, int is_transparent):
         self._extend()
-        self.blocks[self.block_count] = block_model_init(face_data)
+        self.blocks[self.block_count] = block_model_init(face_data, is_transparent)
         self.block_count += 1
 
+    def __len__(self):
+        return self.block_count
 
 
 cdef tuple create_lod0_sub_chunk(
     unsigned int[:, :, :] larger_blocks,
-    char[:, :, :] transparent_array,
-    tuple model_verts,
-    char[:] is_transparent,
+    BlockModelManager block_model_manager,
     long[:] sub_chunk_offset,
 ):
-    cdef int x, y, z, x_, y_, z_, dx, dy, dz, vert_count, vert_end, cull_id, vertex, vertex_attr
-    cdef float shade
-    cdef unsigned int block_id
-    cdef tuple vert_map
+    cdef int x, y, z, x_, y_, z_, dx, dy, dz  # location variables
 
-    cdef float[:, :] vert_table = numpy.empty((ARRAY_SIZE, 12), dtype=numpy.float32)
-    cdef float[:, :] trans_vert_table = numpy.empty((ARRAY_SIZE, 12), dtype=numpy.float32)
-    cdef float[:, :] temp_vert_table
+    # float counters
+    cdef unsigned int vert_start = 0
+    cdef unsigned int trans_vert_start = 0
+    cdef unsigned int vert_count, vert_end, vertex, vertex_attr
+
+    cdef float shade
+    cdef unsigned int block_id, cull_id
+    cdef BlockModel* block_model
+    cdef VertArray* vert_array
+
+    cdef array.array vert_table = array.clone(array.array('f'), ARRAY_SIZE, False)
+    cdef array.array trans_vert_table = array.clone(array.array('f'), ARRAY_SIZE, False)
 
     cdef int size_x = larger_blocks.shape[0] - 2
     cdef int size_y = larger_blocks.shape[1] - 2
@@ -141,8 +153,6 @@ cdef tuple create_lod0_sub_chunk(
     chunk_verts = []
     chunk_verts_translucent = []
 
-    cdef int vert_id = 0
-    cdef int trans_vert_id = 0
     for x in range(size_x):
         for y in range(size_y):
             for z in range(size_z):
@@ -150,62 +160,67 @@ cdef tuple create_lod0_sub_chunk(
                 y_ = y + 1
                 z_ = z + 1
                 block_id = larger_blocks[x_, y_, z_]
-                vert_map = model_verts[block_id]
+                block_model = block_model_manager.blocks[block_id]
                 for cull_id in range(7):
-                    if vert_map[cull_id] is not None:
+                    if block_model.faces[cull_id]:
                         # iterate through each cull direction
                         if cull_id:
                             dx = x_ + CULL_MAP[cull_id][0]
                             dy = y_ + CULL_MAP[cull_id][1]
                             dz = z_ + CULL_MAP[cull_id][2]
                             # If the next block is opaque or both blocks are full transparent blocks, do nothing
-                            if transparent_array[dx, dy, dz] == 0 or transparent_array[dx, dy, dz] == transparent_array[x_, y_, z_] == 1:
+                            if block_model_manager.blocks[larger_blocks[dx, dy, dz]].is_transparent == 0 or \
+                               block_model_manager.blocks[larger_blocks[dx, dy, dz]].is_transparent == block_model_manager.blocks[larger_blocks[x_, y_, z_]].is_transparent == 1:
                                 continue
 
-                        temp_vert_table = vert_map[cull_id]
-                        vert_count = temp_vert_table.shape[0]
+                        vert_array = block_model.faces[cull_id]
+                        vert_count = vert_array.size
 
-                        if is_transparent[block_id] == 1:
-                            vert_end = trans_vert_id+vert_count
-                            if vert_end > ARRAY_SIZE:
-                                chunk_verts_translucent.append(trans_vert_table[:trans_vert_id].copy())
-                                trans_vert_id = 0
-                                vert_end = trans_vert_id+vert_count
-                            trans_vert_table[trans_vert_id:vert_end, :] = temp_vert_table
-                            for vertex in range(trans_vert_id, trans_vert_id + vert_count):
-                                trans_vert_table[vertex, 0] += x + sub_chunk_offset[0]
-                                trans_vert_table[vertex, 1] += y + sub_chunk_offset[1]
-                                trans_vert_table[vertex, 2] += z + sub_chunk_offset[2]
-                                shade = ((trans_vert_table[vertex, 1] / 32) % 2)
-                                if shade > 1:
-                                    shade = - shade + 2
-                                shade = 0.9 + 0.2 * shade
-                                for vertex_attr in range(9, 12):
-                                    trans_vert_table[vertex, vertex_attr] *= shade
-                            trans_vert_id += vert_count
+                        if block_model.is_transparent == 1:
+                            pass
+                            # vert_end = trans_vert_start+vert_count
+                            # if vert_end > ARRAY_SIZE:
+                            #     chunk_verts_translucent.append(trans_vert_table[:trans_vert_start].copy())
+                            #     trans_vert_start = 0
+                            #     vert_end = trans_vert_start+vert_count
+                            # trans_vert_table[trans_vert_start:vert_end, :] = temp_vert_table
+                            # for vertex in range(trans_vert_start, trans_vert_start + vert_count):
+                            #     trans_vert_table[vertex, 0] += x + sub_chunk_offset[0]
+                            #     trans_vert_table[vertex, 1] += y + sub_chunk_offset[1]
+                            #     trans_vert_table[vertex, 2] += z + sub_chunk_offset[2]
+                            #     shade = ((trans_vert_table[vertex, 1] / 32) % 2)
+                            #     if shade > 1:
+                            #         shade = - shade + 2
+                            #     shade = 0.9 + 0.2 * shade
+                            #     for vertex_attr in range(9, 12):
+                            #         trans_vert_table[vertex, vertex_attr] *= shade
+                            # trans_vert_start += vert_count * ATTR_COUNT
                         else:
-                            vert_end = vert_id+vert_count
+                            vert_end = vert_start+vert_count
                             if vert_end > ARRAY_SIZE:
-                                chunk_verts.append(vert_table[:vert_id].copy())
-                                vert_id = 0
-                                vert_end = vert_id+vert_count
-                            vert_table[vert_id:vert_end, :] = temp_vert_table
-                            for vertex in range(vert_id, vert_id + vert_count):
-                                vert_table[vertex, 0] += x + sub_chunk_offset[0]
-                                vert_table[vertex, 1] += y + sub_chunk_offset[1]
-                                vert_table[vertex, 2] += z + sub_chunk_offset[2]
-                                shade = ((vert_table[vertex, 1] / 32) % 2)
+                                chunk_verts.append(vert_table[:vert_start].copy())
+                                vert_start = 0
+                                vert_end = vert_count
+                            memcpy(&vert_table.data.as_floats[vert_start], vert_array.arr, vert_count * sizeof(float))
+                            for vertex in range(vert_start, vert_end, ATTR_COUNT):
+                                vert_table.data.as_floats[vertex + 0] += sub_chunk_offset[0] + x
+                                vert_table.data.as_floats[vertex + 1] += sub_chunk_offset[1] + y
+                                vert_table.data.as_floats[vertex + 2] += sub_chunk_offset[2] + z
+                                shade = ((vert_table.data.as_floats[vertex + 1] / 32) % 2)
                                 if shade > 1:
                                     shade = - shade + 2
                                 shade = 0.9 + 0.2 * shade
                                 for vertex_attr in range(9, 12):
-                                    vert_table[vertex, vertex_attr] *= shade
-                            vert_id += vert_count
+                                    vert_table.data.as_floats[vertex + vertex_attr] *= shade
+                            vert_start += vert_count
 
-    if vert_id:
-        chunk_verts.append(vert_table[:vert_id].copy())
-    if trans_vert_id:
-        chunk_verts_translucent.append(trans_vert_table[:trans_vert_id].copy())
+    if vert_start:
+        # chunk_verts.append(vert_table[:vert_start].copy())
+        chunk_verts.append(numpy.array(vert_table[:vert_start], dtype=numpy.float32))
+    if trans_vert_start:
+        # chunk_verts_translucent.append(trans_vert_table[:trans_vert_start].copy())
+        chunk_verts_translucent.append(numpy.array(trans_vert_table[:trans_vert_start], dtype=numpy.float32))
+
     return chunk_verts, chunk_verts_translucent
 
 
@@ -216,77 +231,46 @@ def create_lod0_chunk(
     block_palette,
     vert_len,  # should be 12
 ):
-    if not hasattr(resource_pack, "block_model_verts"):
+    if not hasattr(resource_pack, "block_model_manager"):
         # TODO: set up a proper location for these
-        resource_pack.block_model_verts = ()
-    if not hasattr(resource_pack, "block_is_transparent"):
-        resource_pack.block_is_transparent = numpy.zeros(0, dtype=numpy.uint8)
-    model_verts = resource_pack.block_model_verts
-    is_transparent = resource_pack.block_is_transparent
+        resource_pack.block_model_manager = BlockModelManager()
 
+    block_model_manager = resource_pack.block_model_manager
+
+    done_count = len(block_model_manager)
     state_count = len(block_palette)
-    done_count = min(len(model_verts), len(is_transparent))
 
-    if state_count != done_count:
+    for block_id in range(done_count, state_count):
         # more block states have been added
 
-        model_verts = model_verts[:done_count]
-        is_transparent = is_transparent[:done_count]
-
-        models = tuple(
-            resource_pack.get_block_model(
-                block_palette[block_temp_id]
-            )
-            for block_temp_id in range(done_count, state_count)
+        model = resource_pack.get_block_model(
+            block_palette[block_id]
         )
-        resource_pack.block_is_transparent = is_transparent = numpy.concatenate(
-            (
-            is_transparent,
-            numpy.array([model.is_transparent for model in models], dtype=numpy.uint8)
-            ),
-            None,
-            dtype=numpy.uint8
-        )
-        texture_bounds = {
-            texture_path: resource_pack.texture_bounds(texture_path)
-            for model in models
-            for texture_path in model.textures
-        }
-        block_model_verts_new = []
-        for model in models:
-            vert_map = [None]*7
-            for py_cull_dir in model.faces.keys():
-                if py_cull_dir is None:
-                    cull_id = 0
-                elif py_cull_dir in CULL_STR_INDEX:
-                    cull_id = CULL_STR_INDEX[py_cull_dir]
-                else:
-                    continue
+        vert_map = {}
+        for py_cull_dir in model.faces.keys():
+            if py_cull_dir in CULL_STR_INDEX:
                 # the vertices in model space
                 verts = model.verts[py_cull_dir].reshape((-1, 3))
                 tverts = model.texture_coords[py_cull_dir].reshape((-1, 2))
                 faces = model.faces[py_cull_dir]
 
                 py_vert_table = numpy.zeros(
-                    (faces.size, vert_len), dtype=numpy.float32
+                    (faces.size, ATTR_COUNT), dtype=numpy.float32
                 )
                 py_vert_table[:, :3] = verts[faces]
                 py_vert_table[:, 3:5] = tverts[faces]
 
                 vert_index = 0
                 for texture_index in model.texture_index[py_cull_dir]:
-                    py_vert_table[vert_index : vert_index + 3, 5:9] = texture_bounds[model.textures[texture_index]]
+                    py_vert_table[vert_index : vert_index + 3, 5:9] = resource_pack.texture_bounds(model.textures[texture_index])
                     vert_index += 3
 
                 py_vert_table[:, 9:12] = (
                     model.tint_verts[py_cull_dir].reshape((-1, 3))[faces]
                     * _brightness_multiplier[py_cull_dir]
                 )
-                vert_map[cull_id] = py_vert_table
-            block_model_verts_new.append(tuple(vert_map))
-
-        resource_pack.block_model_verts = model_verts = model_verts + tuple(block_model_verts_new)
-
+                vert_map[py_cull_dir] = py_vert_table
+        block_model_manager.add_block(vert_map, model.is_transparent)
     chunk_verts = []
     chunk_verts_translucent = []
 
@@ -296,9 +280,7 @@ def create_lod0_chunk(
 
         chunk_verts_, chunk_verts_translucent_ = create_lod0_sub_chunk(
             block_array,
-            is_transparent[block_array],
-            model_verts,
-            is_transparent,
+            block_model_manager,
             sub_chunk_offset,
         )
         chunk_verts += chunk_verts_
