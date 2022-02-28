1acc12fb93ee34a879456c56e9e28317c7b4ef88
Vladimir Romashchenko <eaglesemanation@gmail.com>
2022-02-27 22:38:56 -0500

    feat: Define entry-point
    This will automatically create executable script that runs 'main' function and add it to a folder that should be a part of PATH env var. This is useful for tools like pipx

--- amulet_map_editor/__init__.py
+++ amulet_map_editor/__init__.py
@@ -5,3 +5,46 @@ from ._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 from amulet_map_editor.api import config as CONFIG, log, lang
+
+
+def main():
+    try:
+        import sys
+
+        if sys.version_info[:2] < (3, 7):
+            raise Exception("Must be using Python 3.7+")
+
+        import os
+        import traceback
+        import wx
+        from amulet_map_editor import log
+        from amulet_map_editor.api.framework import AmuletApp
+
+        if sys.platform == "linux" and wx.VERSION >= (4, 1, 1):
+            # bug 247
+            os.environ["PYOPENGL_PLATFORM"] = "egl"
+
+    except Exception as e:
+        err = ""
+        try:
+            import traceback
+
+            err += traceback.format_exc()
+        except:
+            pass
+        err += (
+            f"Failed to import requirements. Check that you extracted correctly.\n{e}"
+        )
+        print(err)
+        with open("./logs/amulet_map_editor_.log", "w") as f:
+            f.write(err)
+        input("Press ENTER to continue.")
+    else:
+        try:
+            app = AmuletApp(0)
+            app.MainLoop()
+        except Exception as e:
+            log.critical(
+                f"Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue. \n{traceback.format_exc()}"
+            )
+            input("Press ENTER to continue.")
--- amulet_map_editor/__main__.py
+++ amulet_map_editor/__main__.py
@@ -1,43 +1,6 @@
 #!/usr/bin/env python3
 
+from . import main
+
 if __name__ == "__main__":
-    try:
-        import sys
-
-        if sys.version_info[:2] < (3, 7):
-            raise Exception("Must be using Python 3.7+")
-
-        import os
-        import traceback
-        import wx
-        from amulet_map_editor import log
-        from amulet_map_editor.api.framework import AmuletApp
-
-        if sys.platform == "linux" and wx.VERSION >= (4, 1, 1):
-            # bug 247
-            os.environ["PYOPENGL_PLATFORM"] = "egl"
-
-    except Exception as e:
-        err = ""
-        try:
-            import traceback
-
-            err += traceback.format_exc()
-        except:
-            pass
-        err += (
-            f"Failed to import requirements. Check that you extracted correctly.\n{e}"
-        )
-        print(err)
-        with open("./logs/amulet_map_editor_.log", "w") as f:
-            f.write(err)
-        input("Press ENTER to continue.")
-    else:
-        try:
-            app = AmuletApp(0)
-            app.MainLoop()
-        except Exception as e:
-            log.critical(
-                f"Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue. \n{traceback.format_exc()}"
-            )
-            input("Press ENTER to continue.")
+    main()
