bfaaded6485da5103e2b142be69465ca716a2040
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-07-29 11:10:47 +0100

    Reformatted

--- amulet_map_editor/api/_log.py
+++ amulet_map_editor/api/_log.py
@@ -26,7 +26,10 @@ def _init_logging():
     os.makedirs(logs_path, exist_ok=True)
     # remove all log files older than a week
     for path in glob.glob(os.path.join(logs_path, "*.log")):
-        if os.path.isfile(path) and os.path.getmtime(path) < time.time() - 3600 * 24 * 7:
+        if (
+            os.path.isfile(path)
+            and os.path.getmtime(path) < time.time() - 3600 * 24 * 7
+        ):
             os.remove(path)
 
     file_handler = logging.FileHandler(
