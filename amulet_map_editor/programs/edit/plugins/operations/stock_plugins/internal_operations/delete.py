60c039a9033ab7de3544566984b9aefad6e945fc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-09-27 10:39:45 +0100

    Set/delete block entities in fill, replace and delete operations
    These operations did not previously overwrite the block entities leading to some odd states

--- amulet_map_editor/programs/edit/plugins/operations/stock_plugins/internal_operations/delete.py
+++ amulet_map_editor/programs/edit/plugins/operations/stock_plugins/internal_operations/delete.py
@@ -20,6 +20,21 @@ def delete(
 
     for chunk, slices, _ in world.get_chunk_slice_box(dimension, selection, False):
         chunk.blocks[slices] = internal_id
+
+        chunk_x, chunk_z = chunk.coordinates
+        chunk_x *= 16
+        chunk_z *= 16
+        x_min = chunk_x + slices[0].start
+        y_min = slices[1].start
+        z_min = chunk_z + slices[2].start
+        x_max = chunk_x + slices[0].stop
+        y_max = slices[1].stop
+        z_max = chunk_z + slices[2].stop
+
+        for x, y, z in list(chunk.block_entities.keys()):
+            if x_min <= x < x_max and y_min <= y < y_max and z_min <= z < z_max:
+                chunk.block_entities.pop((x, y, z))
+
         chunk.changed = True
         count += 1
         yield count / iter_count
