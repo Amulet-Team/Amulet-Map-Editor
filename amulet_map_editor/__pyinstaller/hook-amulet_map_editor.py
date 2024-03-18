38ccc144edde165f4ec8cc0fb444e0f987f83438
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-03-18 13:26:26 +0000

    Include PIL submodules

--- amulet_map_editor/__pyinstaller/hook-amulet_map_editor.py
+++ amulet_map_editor/__pyinstaller/hook-amulet_map_editor.py
@@ -1,4 +1,4 @@
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
-hiddenimports = collect_submodules("amulet_map_editor")
+hiddenimports = collect_submodules("amulet_map_editor") + collect_submodules("PIL")
 datas = collect_data_files("amulet_map_editor")
