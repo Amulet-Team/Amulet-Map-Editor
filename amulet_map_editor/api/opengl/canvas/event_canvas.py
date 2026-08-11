399a7843d1c26733987a761a3330126f70e63e7b
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-11 14:41:07 +0100

    Add meta key down handler
    The current implementation has multiple handlers that all need to run.
    The event is only skipped if none of them handle the key.
    This adds a meta handler for key down events that calls the other handlers and skips the event if none of the handlers handled the event.

--- amulet_map_editor/api/opengl/canvas/event_canvas.py
+++ amulet_map_editor/api/opengl/canvas/event_canvas.py
@@ -25,6 +25,7 @@ class EventCanvas(BaseCanvas):
         """Unbind all events.
         We are allowing users to bind custom events so we should have a way to reset what is bound.
         """
+        # Disconnect all handlers
         for event, data in self._bound_events.items():
             for handler, source in data:
                 if source is None:
@@ -42,15 +43,49 @@ class EventCanvas(BaseCanvas):
 
     def Bind(self, event, handler, source=None, id=wx.ID_ANY, id2=wx.ID_ANY):
         """Bind an event to the canvas."""
-        self._bound_events.setdefault(event, []).append((handler, source))
-        super().Bind(event, handler, source, id, id2)
+        # Store the event handler and source
+        event_group = self._bound_events.setdefault(event, [])
+        event_group.append((handler, source))
+
+        if event == wx.EVT_KEY_DOWN:
+            # If this is the first key down handler, bind the key down event
+            if len(event_group) == 1:
+                super().Bind(wx.EVT_KEY_DOWN, self._on_key_down)
+        else:
+            super().Bind(event, handler, source, id, id2)
 
     def Unbind(
         self, event, source=None, id=wx.ID_ANY, id2=wx.ID_ANY, handler=None
     ) -> bool:
         """Unbind an event from the canvas."""
         try:
-            self._bound_events[event].remove((handler, source))
+            # Try and remove the event handler and source
+            event_group = self._bound_events[event]
+            event_group.remove((handler, source))
         except (KeyError, ValueError):
             pass
+        else:
+            # If there are no more handlers for the key down event, unbind it
+            if event == wx.EVT_KEY_DOWN and not event_group:
+                super().Unbind(wx.EVT_KEY_DOWN, self._on_key_down)
+                return True
+
+        # Remove the handler
         return super().Unbind(event, source=source, id=id, id2=id2, handler=handler)
+
+    def _on_key_down(self, evt: wx.KeyEvent) -> None:
+        # On macOS if no handler claims the key down event, an alert sound will play.
+        # To get around this, we must have one handler and skip if no handler claims the event.
+        handled = False
+        for handler, source in self._bound_events.get(wx.EVT_KEY_DOWN, []):
+            if source is not None and source != evt.GetEventObject():
+                continue
+            evt.Skip(False)
+            try:
+                handler(evt)
+            except Exception as e:
+                log.exception(f"Failed to handle key down event: {e}")
+            if not evt.GetSkipped():
+                handled = True
+        if not handled:
+            evt.Skip()
